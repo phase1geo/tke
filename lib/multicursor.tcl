bf02c3ac1eddbc52dbb0cbb84f3ce50fed3c37e8
Trevor Williams <phase1geo@gmail.com>
2014-01-14 10:48:58 -0600

    * Changing number insertion mechanism to use a GUI entry field. * Enhancing number parser to (hopefully) allow the user to enter the value using the languages natural number form as a starting number (and do the right thing).

--- lib/multicursor.tcl
+++ lib/multicursor.tcl
@@ -320,8 +320,13 @@ namespace eval multicursor {
     
     variable selected
     
+    # If the number string is a decimal number without a preceding 'd' character, add it now
+    if {[set d_added [regexp {^[0-9]+$} $numstr]]} { 
+      set numstr "d$numstr"
+    }
+    
     # Parse the number string to verify that it's valid
-    if {[regexp {^(.*)((b[0-1]*)(d[0-9]*)|(o[0-7]*)|([xh][0-9a-fA-F]*))$} $numstr -> prefix numstr]} {
+    if {[regexp {^(.*)((b[0-1]*)|(d[0-9]*)|(o[0-7]*)|([xh][0-9a-fA-F]*))$} $numstr -> prefix numstr]} {
       
       # Get the cursors
       set mcursors [lreverse [$txt tag ranges mcursor]]
@@ -351,21 +356,21 @@ namespace eval multicursor {
           foreach {end start} $mcursors {
             set binRep [binary format c $num]
             binary scan $binRep B* binStr
-            $txt insert $start [string trimleft $binStr 0]
+            $txt insert $start [format "%s%s%s%s" $prefix [string index $numstr 0] [string trimleft [string range $binStr 0 end-1] 0] [string index $binStr end]]
             incr num -1
           }
         }
         d {
           set num [expr $num + ($num_mcursors - 1)]
           foreach {end start} $mcursors {
-            $txt insert $start $num
+            $txt insert $start [format "%s%s%d" $prefix [expr {$d_added ? "" : [string index $numstr 0]}] $num]
             incr num -1
           }
         }
         o {
           set num [expr 0o$num + ($num_mcursors - 1)]
           foreach {end start} $mcursors {
-            $txt insert $start [format "0o%o" $num]
+            $txt insert $start [format "%s%s%o" $prefix [string index $numstr 0] $num]
             incr num -1
           }
         }
@@ -373,7 +378,7 @@ namespace eval multicursor {
         x {
           set num [expr 0x$num + ($num_mcursors - 1)]
           foreach {end start} $mcursors {
-            $txt insert $start [format "0x%x" $num]
+            $txt insert $start [format "%s%s%x" $prefix [string index $numstr 0] $num]
             incr num -1
           }
         }
