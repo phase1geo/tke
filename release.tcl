942d05beb95059cdf882f2ac4209578d31a9f80d
Trevor Williams <phase1geo@users.sourceforge.net>
2014-05-27 23:46:53 -0500

    * Adding tke executable on Mac OSX platforms (for command-line usage). * Fixing About window so that it looks good on Mac OSX. * Adding placeholder for release script.

---
+++ release.tcl
@@ -0,0 +1,14 @@
+#!tclsh8.5
+
+######################################################################
+# Name:    release.tcl
+# Author:  Trevor Williams  (phase1geo@gmail.com)
+# Date:    05/27/2014
+# Brief:   Performs a release of TKE, creating packages for Linux and
+#          Mac OSX.  (Windows can be added here if we can get this to
+#          work.
+# Usage:   tclsh8.5 release.tcl
+######################################################################
+
+# TBD
+
