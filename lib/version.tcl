8e79f29166156359aaedd74f3ba8202c965360a9
Trevor Williams <phase1geo@users.sourceforge.net>
2014-08-12 08:19:40 -0500

    * Changing various gutter commands to a singular gutter command with subcommands. * Fixing several bugs in gutter commands within ctext widget.

--- lib/version.tcl
+++ lib/version.tcl
@@ -1,3 +1,3 @@
 set version_major "1"
 set version_minor "1"
-set version_hgid  "450"
+set version_hgid  "451"
--- plugins/test/main.tcl
+++ plugins/test/main.tcl
@@ -3,11 +3,15 @@ namespace eval test {
   proc on_start {} {
     set txt [api::file::add [file join [api::get_plugin_directory] main.tcl] \
       -gutters {{test heart \u2665 {-fg red -onenter test::gutter_on_enter} dollar $ {-fg green}}}]
-    $txt setgutter test heart {2 3} dollar 6
+    $txt gutter set test heart {2 3} dollar 6
   }
   
   proc gutter_on_enter {txt} {
-    puts "HERE!"
+    puts "gutter get:                  [$txt gutter get test]"
+    puts "gutter cget heart -fg:       [$txt gutter cget test heart -fg]"
+    puts "gutter cget heart -onenter:  [$txt gutter cget test heart -onenter]"
+    puts "gutter cget heart -onleave:  [$txt gutter cget test heart -onleave]"
+    puts "gutter names:                [$txt gutter names]"
   }
 
 }
