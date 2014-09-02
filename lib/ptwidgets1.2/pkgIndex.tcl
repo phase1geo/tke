8fec57faaefda252d0b42add2b7d76246ed44df2
Trevor Williams (phase1geo <Trevor Williams (phase1geo@gmail.com)>
2014-09-02 01:01:03 -0500

    * Fixing various issues with whitespace in filenames. * Checkpointing work in Windows.

--- lib/ptwidgets1.2/pkgIndex.tcl
+++ lib/ptwidgets1.2/pkgIndex.tcl
@@ -1,20 +1,42 @@
 #
 # Tcl package index file
 #
-package ifneeded tokenentry  1.2 \
-  "namespace eval tokenentry { proc DIR {} {return [list $dir]} }; source [file join $dir library tokenentry.tcl]"
-package ifneeded tokensearch 1.2 \
-  "namespace eval tokensearch { proc DIR {} {return [list $dir]} }; source [file join $dir library tokensearch.tcl]"
-package ifneeded wmarkentry 1.2 \
-  "namespace eval wmarkentry { proc DIR {} {return [list $dir]} }; source [file join $dir library wmarkentry.tcl]"
-package ifneeded toggleswitch 1.2 \
-  "namespace eval toggleswitch { proc DIR {} {return [list $dir]} }; source [file join $dir library toggleswitch.tcl]"
-package ifneeded carousel 1.2 \
-  "namespace eval carousel { proc DIR {} {return [list $dir]} }; source [file join $dir library carousel.tcl]"
-package ifneeded timeline 1.2 \
-  "namespace eval timeline { proc DIR {} {return [list $dir]} }; source [file join $dir library timeline.tcl]"
-package ifneeded specl 1.2 \
-  "namespace eval specl { proc DIR {} {return [list $dir]} }; source [file join $dir library specl.tcl]"
-package ifneeded tabbar 1.2 \
-  "namespace eval tabbar { proc DIR {} {return [list $dir]} }; source [file join $dir library tabbar.tcl]"
+package ifneeded tokenentry 1.2 [list apply { dir {
+	namespace eval tokenentry [list proc DIR {} [list return $dir]]
+	source [file join $dir library tokenentry.tcl]
+}} $dir]
 
+package ifneeded tokensearch 1.2 [list apply { dir {
+  namespace eval tokensearch [list proc DIR {} [list return $dir]]
+  source [file join $dir library tokensearch.tcl]
+}} $dir]
+
+package ifneeded wmarkentry 1.2 [list apply { dir {
+  namespace eval wmarkentry [list proc DIR {} [list return $dir]]
+  source [file join $dir library wmarkentry.tcl]
+}} $dir]
+
+package ifneeded toggleswitch 1.2 [list apply { dir {
+	namespace eval toggleswitch [list proc DIR {} [list return $dir]]
+	source [file join $dir library toggleswitch.tcl]
+}} $dir]
+
+package ifneeded carousel 1.2 [list apply { dir {
+  namespace eval carousel [list proc DIR {} [list return $dir]]
+  source [file join $dir library carousel.tcl]
+}} $dir]
+
+package ifneeded timeline 1.2 [list apply { dir {
+  namespace eval timeline [list proc DIR {} [list return $dir]]
+  source [file join $dir library timeline.tcl]
+}} $dir]
+
+package ifneeded specl 1.2 [list apply { dir {
+  namespace eval specl [list proc DIR {} [list return $dir]]
+  source [file join $dir library specl.tcl]
+}} $dir]
+
+package ifneeded tabbar 1.2 [list apply { dir {
+  namespace eval tabbar [list proc DIR {} [list return $dir]]
+  source [file join $dir library tabbar.tcl]
+}} $dir]
