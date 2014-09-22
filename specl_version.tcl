87144faeb47671340ce048c9e41f7b701070e65e
Trevor Williams <phase1geo@users.sourceforge.net>
2014-09-21 20:05:41 -0500

    * Fixing tarballing issue in specl.

--- specl_version.tcl
+++ specl_version.tcl
@@ -1,4 +1,4 @@
 set specl::appname   "tke"
-set specl::version   "1.2"
-set specl::rss_url   "http://tke.sourceforge.net/tke"
+set specl::version   "1.3"
+set specl::rss_url   "http://tke.sourceforge.net/releases"
 set specl::icon_path "lib/images/tke_logo_64.gif"
