1151a041158a1e8f8dc59d79bc3695de762b9a7f
Trevor Williams <phase1geo@users.sourceforge.net>
2014-11-29 00:24:24 -0600

    ChangeLog for stable-1.5 release

--- lib/ptwidgets1.2/library/specl_version.tcl
+++
@@ -1,3 +0,0 @@
-set specl::version   "0.9"
-set specl::rss_url   "http://ptwidgets.sourceforge.net/testapp/appcast.xml"
-set specl::icon_path "/Users/trevorw/projects/tke-code/lib/images/tke_logo_64.gif"
