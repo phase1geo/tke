8403af7db74773e3a9d3bc48b05232ba28e3a927
Trevor Williams <phase1geo@users.sourceforge.net>
2015-02-12 08:55:42 -0600

    ChangeLog for devel-1.4.4 release

--- specl_version.tcl
+++ specl_version.tcl
@@ -1,5 +1,5 @@
 set specl::appname      "tke"
-set specl::version      "1.4.3"
-set specl::release      "42"
+set specl::version      "1.4.4"
+set specl::release      "43"
 set specl::rss_url      "http://tke.sourceforge.net/releases"
 set specl::download_url "http://tke.sourceforge.net/releases"
