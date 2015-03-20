fe4ae8a0d3568f1bd8ae73a1e3c5f6077a7e1c64
Trevor Williams <phase1geo@gmail.com>
2015-03-20 17:03:07 -0500

    * More work on test script.

--- lib/test.tcl
+++ lib/test.tcl
@@ -11,10 +11,37 @@ if {![catch { open [file join api.tcl] r } rc]} {
   close $rc
 }
 
-.t tag add diff:A:S:1 1.0 3.end
-.t tag add diff:A:D:4 4.0 5.end
-.t tag add diff:A:S:4 6.0 end
+# THIS CODE DOES WHAT I WANT THE LIBRARY CODE TO DO
+# .t tag add diff:A:S:1 1.0 3.end
+# .t tag add diff:A:D:4 4.0 5.end
+# .t tag add diff:A:S:4 6.0 end
+#
+# .t tag add diff:B:S:1  1.0 10.end
+# .t tag add diff:B:D:11 11.0 14.end
+# .t tag add diff:B:S:11 15.0 end
 
-.t tag add diff:B:S:1  1.0 10.end
-.t tag add diff:B:D:11 11.0 14.end
-.t tag add diff:B:S:11 15.0 end
+.t tag add diff:A:S:1 1.0 end
+.t tag add diff:B:S:1 1.0 end
+
+if {![catch { exec -ignorestderr hg diff api.tcl } rc]} {
+  foreach line [split $rc \n] {
+    if {[regexp {^@@\s+\-(\d+),(\d+)\s+\+(\d+),(\d+)\s+@@$} $line -> lineA linesA lineB linesB]} {
+      set adds 0
+      set subs 0
+      set strA ""
+    } elseif {[regexp {^\+} $line]} {
+      incr adds
+      incr lineB
+    } elseif {[regexp {^\-(.*)$} $line -> str]} {
+      if {$adds > 0} {
+
+      }
+      append strA "$str\n"
+      incr subs
+      incr lineA
+    } elseif {($adds > 0) || ($subs > 0)} {
+      incr lineA
+      incr lineB
+    }
+  }
+}
