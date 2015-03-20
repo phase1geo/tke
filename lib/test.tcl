378f4b603656c2d892880b5fefc0bdb40b7ab3ad
Trevor Williams <phase1geo@gmail.com>
2015-03-20 16:46:27 -0500

    * Fixing issues with linemapDiffUpdate procedure. * Updating test script.

--- lib/test.tcl
+++ lib/test.tcl
@@ -3,7 +3,7 @@ set auto_path [list [pwd] {*}$auto_path]
 package require -exact ctext 5.0
 
 # Create the UI
-pack [ctext .t -linemap 1 -linemap_minwidth 4 -diff_mode 1 -wrap none]
+pack [ctext .t -linemap 1 -linemap_minwidth 2 -diff_mode 1 -wrap none]
 
 # Insert content
 if {![catch { open [file join api.tcl] r } rc]} {
@@ -11,6 +11,10 @@ if {![catch { open [file join api.tcl] r } rc]} {
   close $rc
 }
 
-.t tag add diff:A:1 1.0 3.end
-.t tag add diff:A:4 6.0 end
-.t tag add diff:B:1 1.0 end
+.t tag add diff:A:S:1 1.0 3.end
+.t tag add diff:A:D:4 4.0 5.end
+.t tag add diff:A:S:4 6.0 end
+
+.t tag add diff:B:S:1  1.0 10.end
+.t tag add diff:B:D:11 11.0 14.end
+.t tag add diff:B:S:11 15.0 end
