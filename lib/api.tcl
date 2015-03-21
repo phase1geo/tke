1e7fd32114e4e1ae1ee91b057b890583bfc6f9b7
Trevor Williams <phase1geo@users.sourceforge.net>
2015-03-21 11:47:34 -0500

    * Fixing API.

--- lib/api.tcl
+++ lib/api.tcl
@@ -29,9 +29,6 @@ namespace eval api {
       return $plugin_dir
     }
     
-    # Some extra code
-    set foobar "HERE"
-
   }
 
   ######################################################################
@@ -40,11 +37,11 @@ namespace eval api {
 
     set img_dir [file join $::tke_dir plugins images]
 
-    #if {[$interp issafe]} {
+    if {[$interp issafe]} {
       return [::safe::interpFindInAccessPath $interp $img_dir]
     } else {
       return $img_dir
-    #}
+    }
 
   }
 
