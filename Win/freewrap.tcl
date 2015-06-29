4d4c36d9ae516b347e5d21a96e2cc89e74cbde6c
Trevor Williams <phase1geo@gmail.com>
2015-06-29 00:27:18 -0500

    * Updates to get freewrap build to work.

--- Win/freewrap.tcl
+++ Win/freewrap.tcl
@@ -1,5 +1,5 @@
 proc find_package_helper {dname pkg} {
-  
+
   # If we can find a pkgIndex.tcl file in the dname directory
   # see if it calls an ifneeded for the given package.
   if {[file exists [set pkg_index [file join $dname pkgIndex.tcl]]]} {
@@ -14,30 +14,75 @@ proc find_package_helper {dname pkg} {
       close $rc
     }
   }
-  
+
   # Continue to search in subdirectories
   foreach sdname [glob -nocomplain -directory $dname -types d *] {
     if {[find_package_helper $sdname $pkg]} {
       return 1
     }
   }
-  
+
   return 0
-  
+
 }
 
 proc find_package {pkg} {
-  
+
   foreach dname $::auto_path {
     if {[find_package_helper $dname $pkg]} {
       return 1
     }
   }
-  
+
   return 0
-  
+
 }
 
+proc get_files {parent} {
+
+  set files [list]
+
+  foreach fname [glob -nocomplain -directory $parent *] {
+    if {[file isfile $fname]} {
+      lappend files $fname
+    } elseif {[file isdirectory $fname]} {
+      lappend files {*}[get_files $fname]
+    }
+  }
+
+  return $files
+
+}
+
+proc create_freewrap_files {} {
+
+  set files [list]
+
+  puts -nonewline "Creating freewrap.files...  "
+
+  # Create the freewrap file
+  foreach dir [list data doc lib plugins] {
+    lappend files {*}[get_files [file join $::tke_dir $dir]]
+  }
+
+  # Remove excluded directories
+  foreach exclude [list doc/html lib/tke.tcl] {
+    set files [lsearch -inline -not -all $files [file join $::tke_dir $exclude]*]
+  }
+
+  # Write the files to the given directory
+  if {![catch { open freewrap.files w } rc]} {
+    foreach fname $files {
+      puts $rc $fname
+    }
+    close $rc
+  }
+
+  puts "Done!"
+
+}
+
+set tke_dir       [file normalize [file join [pwd] ..]]
 set new_auto_path $auto_path
 
 foreach pkg [list Tclx] {
@@ -46,11 +91,16 @@ foreach pkg [list Tclx] {
   }
 }
 
-puts "A auto_path: $auto_path"
 set auto_path $new_auto_path
-puts "B auto_path: $auto_path"
 
-if {![catch { exec -ignorestderr freewrap [file join [pwd] lib tke.tcl] -debug -f freewrap.files -i tke.ico } rc]} {
-  puts "Worked!"
+# Create the freewrap.files file with the current directory contents
+create_freewrap_files
+
+puts -nonewline "Running freewrap...  "
+if {![catch { exec -ignorestderr [file join freewrap664 win64 freewrap.exe] [file join $tke_dir lib tke.tcl] -debug -f freewrap.files -i tke.ico } rc]} {
+  puts "Success!"
+} else {
+  puts "Failed!"
+  puts $rc
 }
 
