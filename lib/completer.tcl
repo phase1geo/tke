16f7e1f65f634640c205ef5439955c53928c09ad
Trevor Williams <phase1geo@gmail.com>
2015-08-07 23:33:10 -0500

    * Checkpointing improvements on completer.

--- lib/completer.tcl
+++ lib/completer.tcl
@@ -4,7 +4,7 @@
 # Brief:    Contains namespace handling bracket/string completion.
 
 namespace eval completer {
-  
+
   source [file join $::tke_dir lib ns.tcl]
 
   array set pref_complete    {}
@@ -12,14 +12,14 @@ namespace eval completer {
   array set lang_match_chars {}
 
   trace add variable [ns preferences]::prefs(Editor/AutoMatchChars) write [ns completer]::handle_auto_match_chars
-  
+
   ######################################################################
   # Handles any changes to the Editor/AutoMatchChars preference value.
   proc handle_auto_match_chars {name1 name2 op} {
 
     variable pref_complete
     variable lang_match_chars
-    
+
     # Populate the pref_complete array with the values from the preferences file
     array set pref_complete {
       square 0
@@ -29,7 +29,7 @@ namespace eval completer {
       double 0
       single 0
     }
-    
+
     foreach value [[ns preferences]::get Editor/AutoMatchChars] {
       set pref_complete($value) 1
     }
@@ -40,18 +40,18 @@ namespace eval completer {
     }
 
   }
-  
+
   ######################################################################
   # Sets the auto-match characters based on the current language.
   proc set_auto_match_chars {txt matchchars} {
-    
+
     variable lang_match_chars
     variable pref_complete
     variable complete
-    
+
     # Save the language-specific match characters
     set lang_match_chars($txt) $matchchars
-    
+
     # Initialize the complete array for the given text widget
     array set complete [list \
       $txt,square 0 \
@@ -61,20 +61,20 @@ namespace eval completer {
       $txt,double 0 \
       $txt,single 0 \
     ]
-      
+
     # Combine the language-specific match chars with preference chars
     foreach match_char $lang_match_chars($txt) {
       if {$pref_complete($match_char)} {
         set complete($txt,$match_char) 1
       }
     }
-      
+
   }
- 
+
   ######################################################################
   # Adds bindings to the given text widget.
   proc add_bindings {txt} {
-    
+
     bind comp$txt <Key-bracketleft>  "[ns completer]::add_square %W left"
     bind comp$txt <Key-bracketright> "if {\[[ns completer]::add_square %W right\]} { break }"
     bind comp$txt <Key-braceleft>    "[ns completer]::add_curly %W left"
@@ -86,39 +86,39 @@ namespace eval completer {
     bind comp$txt <Key-quotedbl>     "if {\[[ns completer]::add_double %W\]} { break }"
     bind comp$txt <Key-quoteright>   "if {\[[ns completer]::add_single %W\]} { break }"
     bind comp$txt <BackSpace>        "[ns completer]::handle_delete %W"
-    
+
     # Add the bindings
     set text_index [lsearch [bindtags $txt.t] Text]
     bindtags $txt.t [linsert [bindtags $txt.t] $text_index comp$txt]
-    
+
     # Make sure that the complete array is initialized for the text widget
     # in case there is no language
     set_auto_match_chars $txt.t {}
-      
+
   }
-  
+
   ######################################################################
   # Returns true if a closing character should be automatically added.
   proc add_closing {txt} {
-    
+
     # Get the character at the insertion cursor
     set ch [$txt get insert]
-    
+
     if {[string is space $ch] || ($ch eq "\}") || ($ch eq "\)") || ($ch eq ">") || ($ch eq "]")} {
       return 1
     }
-    
+
     return 0
-    
+
   }
-  
+
   ######################################################################
   # Handles a square bracket.
   proc add_square {txt side} {
-    
+
     variable complete
 
-    if {$complete($txt,square) && ![ctext::inCommentString $txt insert]} {
+    if {$complete($txt,square) && ![ctext::inComment $txt insert]} {
       if {$side eq "right"} {
         if {([$txt get insert] eq "\]") && ![ctext::isEscaped $txt insert]} {
           $txt mark set insert "insert+1c"
@@ -133,18 +133,18 @@ namespace eval completer {
         $txt mark set insert $ins
       }
     }
-    
+
     return 0
-    
+
   }
-  
+
   ######################################################################
   # Handles a curly bracket.
   proc add_curly {txt side} {
 
     variable complete
-    
-    if {$complete($txt,curly) && ![ctext::inCommentString $txt insert]} {
+
+    if {$complete($txt,curly) && ![ctext::inComment $txt insert]} {
       if {$side eq "right"} {
         if {([$txt get insert] eq "\}") && ![ctext::isEscaped $txt insert]} {
           $txt mark set insert "insert+1c"
@@ -163,14 +163,14 @@ namespace eval completer {
     return 0
 
   }
-  
+
   ######################################################################
   # Handles an angled bracket.
   proc add_angled {txt side} {
 
     variable complete
-    
-    if {$complete($txt,angled) && ![ctext::inCommentString $txt insert]} {
+
+    if {$complete($txt,angled) && ![ctext::inComment $txt insert]} {
       if {$side eq "right"} {
         if {([$txt get insert] eq ">") && ![ctext::isEscaped $txt insert]} {
           $txt mark set insert "insert+1c"
@@ -187,16 +187,16 @@ namespace eval completer {
     }
 
     return 0
-    
+
   }
-  
+
   ######################################################################
   # Handles a parenthesis.
   proc add_paren {txt side} {
 
     variable complete
-    
-    if {$complete($txt,paren) && ![ctext::inCommentString $txt insert]} {
+
+    if {$complete($txt,paren) && ![ctext::inComment $txt insert]} {
       if {$side eq "right"} {
         if {([$txt get insert] eq ")") && ![ctext::isEscaped $txt insert]} {
           $txt mark set insert "insert+1c"
@@ -211,11 +211,11 @@ namespace eval completer {
         $txt mark set insert $ins
       }
     }
-    
+
     return 0
 
   }
-  
+
   ######################################################################
   # Handles a double-quote character.
   proc add_double {txt} {
@@ -223,7 +223,7 @@ namespace eval completer {
     variable complete
 
     if {$complete($txt,double)} {
-      if {[ctext::inCommentString $txt insert]} {
+      if {[ctext::inString $txt insert]} {
         if {([$txt get insert] eq "\"") && ![ctext::isEscaped $txt insert]} {
           $txt mark set insert "insert+1c"
           ctext::matchQuote [winfo parent $txt]
@@ -231,7 +231,7 @@ namespace eval completer {
         }
       } else {
         set ins [$txt index insert]
-        if {![ctext::inCommentString $txt end-1c]} {
+        if {![ctext::inComment $txt insert]} {
           $txt insert insert "\""
         }
         $txt mark set insert $ins
@@ -239,9 +239,9 @@ namespace eval completer {
     }
 
     return 0
-    
+
   }
-  
+
   ######################################################################
   # Handles a single-quote character.
   proc add_single {txt} {
@@ -249,14 +249,14 @@ namespace eval completer {
     variable complete
 
     if {$complete($txt,single)} {
-      if {[ctext::inCommentString $txt insert]} {
+      if {[ctext::inSingleQuote $txt insert]} {
         if {([$txt get insert] eq "'") && ![ctext::isEscaped $txt insert]} {
           $txt mark set insert "insert+1c"
           return 1
         }
       } else {
         set ins [$txt index insert]
-        if {![ctext::inCommentString $txt end-1c]} {
+        if {![ctext::inComment $txt insert]} {
           $txt insert insert "'"
         }
         $txt mark set insert $ins
@@ -273,7 +273,7 @@ namespace eval completer {
 
     variable complete
 
-    if {![ctext::inCommentString $txt insert-2c]} {
+    if {![ctext::inComment $txt insert-2c]} {
       switch [$txt get insert-1c insert+1c] {
         "\[\]" {
           if {$complete($txt,square)} {
@@ -309,5 +309,5 @@ namespace eval completer {
     }
 
   }
-  
+
 }
