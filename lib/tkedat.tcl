79ae8f32035decebcec86603334ebe766189a4de
Trevor Williams <phase1geo@users.sourceforge.net>
2015-09-03 02:10:44 -0500

    * Adding support for new -lang option to add_buffer to specify starting language syntax to use for buffer. * Fixing lots of issues with new preferences handling. * Saving/loading all preference information without comments (insert comments when editing only -- comments come from base preferences file). * Split out tkedat read into both read and parse string procedures (for improved flexibility). * Adding include_comments option to the tkedat::write procedure.

--- lib/tkedat.tcl
+++ lib/tkedat.tcl
@@ -55,79 +55,84 @@ namespace eval tkedat {
   # that no Tcl commands are called.
   proc read {fname {include_comments 1}} {
 
-    array set contents [list]
-
+    set contents ""
+    
     if {![catch { open $fname r } rc]} {
+      set contents [::read $rc]  
+      close $rc
+    }
+    
+    return [parse $contents $include_comments]
+    
+  }
+  
+  ######################################################################
+  # Parses the given string for tkedat formatted text.
+  proc parse {str {include_comments 1}} {
 
-      set comments [list]
-      set value_ip 0
-      set linenum  1
+    array set contents [list]
+    
+    set comments [list]
+    set value_ip 0
+    set linenum  1
+ 
+    foreach line [split $str \n] {
+ 
+      if {!$value_ip && [regexp {^\s*#(.*)$} $line -> comment]} {
+ 
+        lappend comments $comment
+ 
+      } elseif {!$value_ip && [regexp -indices {^\s*(\{[^\}]*\}|\S+)\s+(\{.*)$} $line -> key value]} {
 
-      foreach line [split [::read $rc] \n] {
-
-        if {!$value_ip && [regexp {^\s*#(.*)$} $line -> comment]} {
-
-          lappend comments $comment
-
-        } elseif {!$value_ip && [regexp -indices {^\s*(\{[^\}]*\}|\S+)\s+(\{.*)$} $line -> key value]} {
-
-          set key [string map {\{ {} \} {}} [string range $line {*}$key]]
-
-          if {[bracket_count $line $linenum [lindex $value 0]] == 0} {
-          	set contents($key) [string range [string trim [string range $line {*}$value]] 1 end-1]
-            if {[regexp {\[.*\]} $contents($key)]} {
-              unset contents($key)
-            } elseif {$include_comments} {
-              set contents($key,comment) $comments
-            }
-            set comments [list]
-          } else {
-          	set contents($key) [string range [string range $line {*}$value] 1 end]
-            set value_ip 1
-          }
-
-        } elseif {!$value_ip && [regexp {^\s*(\{[^\}]*\}|\S+)\s+(\S+)$} $line -> key value]} {
-
-          set key [string map {\{ {} \} {}} $key]
-          set contents($key) [string trim $value]
-
+        set key [string map {\{ {} \} {}} [string range $line {*}$key]]
+ 
+        if {[bracket_count $line $linenum [lindex $value 0]] == 0} {
+        	set contents($key) [string range [string trim [string range $line {*}$value]] 1 end-1]
           if {[regexp {\[.*\]} $contents($key)]} {
             unset contents($key)
           } elseif {$include_comments} {
             set contents($key,comment) $comments
           }
           set comments [list]
-
-        } elseif {$value_ip} {
-
-          if {[bracket_count $line $linenum 0] == 0} {
-            append contents($key) " [string range [string trim $line] 0 end-1]"
-            if {[regexp {\[.*\]} $contents($key)]} {
-              unset contents($key)
-            } elseif {$include_comments} {
-              set contents($key,comment) $comments
-            }
-            set comments [list]
-            set value_ip 0
-          } else {
-          	if {$include_comments} {
-              append contents($key) "$line\n"
-            } else {
-              append contents($key) " [string trim $line]"
-            }
-          }
-
+        } else {
+        	set contents($key) [string range [string range $line {*}$value] 1 end]
+          set value_ip 1
         }
 
-        incr linenum
+      } elseif {!$value_ip && [regexp {^\s*(\{[^\}]*\}|\S+)\s+(\S+)$} $line -> key value]} {
+
+        set key [string map {\{ {} \} {}} $key]
+        set contents($key) [string trim $value]
+ 
+        if {[regexp {\[.*\]} $contents($key)]} {
+          unset contents($key)
+        } elseif {$include_comments} {
+          set contents($key,comment) $comments
+        }
+        set comments [list]
+ 
+      } elseif {$value_ip} {
+ 
+        if {[bracket_count $line $linenum 0] == 0} {
+          append contents($key) " [string range [string trim $line] 0 end-1]"
+          if {[regexp {\[.*\]} $contents($key)]} {
+            unset contents($key)
+          } elseif {$include_comments} {
+            set contents($key,comment) $comments
+          }
+          set comments [list]
+          set value_ip 0
+        } else {
+         	if {$include_comments} {
+            append contents($key) "$line\n"
+          } else {
+            append contents($key) " [string trim $line]"
+          }
+        }
 
       }
 
-      close $rc
-
-    } else {
-
-      return -code error [msgcat::mc "Unable to open %s for reading" $fname]
+      incr linenum
 
     }
 
@@ -138,7 +143,7 @@ namespace eval tkedat {
   ######################################################################
   # Writes the given array to the given tkedat file, adding the comments
   # back to the file.
-  proc write {fname contents} {
+  proc write {fname contents {include_comments 1}} {
 
     if {![catch { open $fname w } rc]} {
 
@@ -146,12 +151,16 @@ namespace eval tkedat {
 
       foreach name [lsort [array names content]] {
         if {![regexp {,comment$} $name]} {
-          if {[info exists content($name,comment)]} {
-            foreach comment $content($name,comment) {
-              puts $rc "#$comment"
+          if {$include_comments} {
+            if {[info exists content($name,comment)]} {
+              foreach comment $content($name,comment) {
+                puts $rc "#$comment"
+              }
             }
+            puts $rc "\n{$name} {$content($name)}\n"
+          } else {
+            puts $rc "{$name} {$content($name)}"
           }
-          puts $rc "\n{$name} {$content($name)}\n"
         }
       }
 
