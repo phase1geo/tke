754d4b39ec752bbde3d4aa4773b962f86183fc9f
Trevor Williams <phase1geo@users.sourceforge.net>
2015-09-20 22:25:02 -0500

    * Improving language translation automation.

--- data/msgs/update.tcl
+++ data/msgs/update.tcl
@@ -1,9 +1,32 @@
 #!tclsh8.5
 
-foreach msg [glob *.msg] {
+proc usage {} {
   
-  set lang [file rootname $msg]
+  puts "tclsh8.5 update.tcl -- <options>"
+  puts ""
+  puts "Options:"
+  puts "  -h  Display this usage information"
+  puts "  -a  Update all existing translations automatically"
   
-  exec wish8.5 [file join .. .. lib lang.tcl] $lang
+  exit
   
 }
+
+set i         0
+set lang_args [list]
+
+while {$i < $argc} {
+  switch -exact -- [lindex $argv $i] {
+    -h { usage }
+    -a { lappend lang_args -auto }
+  }
+  incr i
+}
+
+set langs [list]
+foreach msg [glob *.msg] {
+  lappend langs [file rootname $msg]
+}
+  
+
+exec wish8.5 [file join .. .. lib lang.tcl] {*}$lang_args {*}$langs
