dc28a4885a59535cf123b1a16b042aa15a9a9635
Trevor Williams <phase1geo@users.sourceforge.net>
2015-09-25 23:25:05 -0500

    ChangeLog for devel-1.8.28 release

--- specl_version.tcl
+++ specl_version.tcl
@@ -1,5 +1,5 @@
 set specl::appname      "tke"
-set specl::version      "1.8.27"
-set specl::release      "157"
+set specl::version      "1.8.28"
+set specl::release      "158"
 set specl::rss_url      "http://tke.sourceforge.net/releases"
 set specl::download_url "http://tke.sourceforge.net/releases"
