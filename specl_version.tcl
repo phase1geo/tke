eb67c90e054b3de30e506ac2c39942eedaff9f7b
Trevor Williams <phase1geo@users.sourceforge.net>
2015-12-07 17:40:35 -0600

    ChangeLog for stable-2.0 release

--- specl_version.tcl
+++ specl_version.tcl
@@ -1,5 +1,5 @@
 set specl::appname      "tke"
-set specl::version      "1.9.17"
-set specl::release      "179"
+set specl::version      "2.0"
+set specl::release      "180"
 set specl::rss_url      "http://tke.sourceforge.net/releases"
 set specl::download_url "http://tke.sourceforge.net/releases"
