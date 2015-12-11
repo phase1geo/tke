02a811edc5c88b6bc1b01af3f23dc8192c1df5dd
Trevor Williams <phase1geo@users.sourceforge.net>
2015-12-10 22:58:13 -0600

    * Updating Info.plist release from 2.0 to 2.1. * Adding -n option to AppMain.tcl * Adding "New Window" to File menu.

--- MacOSX/Tke.app/Contents/Resources/Scripts/AppMain.tcl
+++ MacOSX/Tke.app/Contents/Resources/Scripts/AppMain.tcl
@@ -3,20 +3,20 @@ if {[string first "-psn" [lindex $argv 0]] == 0} {
 }
 
 if {[llength $argv] > 0} {
-  
+
   set argv [lassign $argv argv0]
   if {[catch { source $argv0 }]} {
     puts $errorInfo
   }
-  
+
 } else {
 
   # Avoid populating the sidebar on startup
-  set argv [concat "-nosb" $argv]
+  set argv [concat "-nosb" "-n" $argv]
   incr argc
 
   if {[catch { source [file join [file dirname $argv0] tke lib tke.tcl] }]} {
     puts $errorInfo
   }
-  
+
 }
