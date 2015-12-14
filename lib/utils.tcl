2f15115f688e8562ac1663ea18889a80d0a7c3d2
Trevor Williams <phase1geo@users.sourceforge.net>
2015-12-13 23:55:10 -0600

    * Fixing issue with utils::stacktrace procedure. * Adding support for value substitutions in find/replace regular expression handling. * Fixing issue with saving an untitled buffer with a name in the Vim command field.

--- lib/utils.tcl
+++ lib/utils.tcl
@@ -74,25 +74,28 @@ namespace eval utils {
   proc stacktrace {} {
 
     set stack "Stack trace:\n"
-    for {set i 1} {$i < [info level]} {incr i} {
-      set lvl [info level -$i]
-      set pname [lindex $lvl 0]
-      if {[namespace which -command $pname] eq ""} {
-        for {set j [expr $i + 1]} {$j < [info level]} {incr j} {
-          if {[namespace which -command [lindex [info level -$j] 0]] ne ""} {
-            set pname "[namespace qualifiers [lindex [info level -$j] 0]]::$pname"
-            break
+
+    catch {
+      for {set i 1} {$i < [info level]} {incr i} {
+        set lvl [info level -$i]
+        set pname [lindex $lvl 0]
+        if {[namespace which -command $pname] eq ""} {
+          for {set j [expr $i + 1]} {$j < [info level]} {incr j} {
+            if {[namespace which -command [lindex [info level -$j] 0]] ne ""} {
+              set pname "[namespace qualifiers [lindex [info level -$j] 0]]::$pname"
+              break
+            }
           }
         }
-      }
-      append stack [string repeat " " $i]$pname
-      foreach value [lrange $lvl 1 end] arg [info args $pname] {
-        if {$value eq ""} {
-          info default $pname $arg value
+        append stack [string repeat " " $i]$pname
+        foreach value [lrange $lvl 1 end] arg [info args $pname] {
+          if {$value eq ""} {
+            info default $pname $arg value
+          }
+          append stack " $arg='$value'"
         }
-        append stack " $arg='$value'"
-      }
-      append stack \n
+        append stack \n
+      } 
     }
 
     return $stack
