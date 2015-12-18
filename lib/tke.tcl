76a19038e1d6af77a8b44e3af36b9d2055018bec
Trevor Williams <phase1geo@users.sourceforge.net>
2015-12-18 00:43:25 -0600

    * Adding start of in-app BIST.

--- lib/tke.tcl
+++ lib/tke.tcl
@@ -111,6 +111,7 @@ source [file join $tke_dir lib diff.tcl]
 source [file join $tke_dir lib sessions.tcl]
 source [file join $tke_dir lib search.tcl]
 source [file join $tke_dir lib scroller.tcl]
+source [file join $tke_dir lib bist.tcl]
 
 if {[tk windowingsystem] eq "aqua"} {
   source [file join $tke_dir lib windowlist.tcl]
