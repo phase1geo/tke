bab7bfd546a3c774c24d4796921eff21d2cb904f
Trevor Williams <phase1geo@users.sourceforge.net>
2015-12-27 00:18:45 -0600

    * Adding ability to open shown files in the sidebar.

--- lib/launcher.tcl
+++ lib/launcher.tcl
@@ -530,6 +530,14 @@ namespace eval launcher {
   }
 
   ######################################################################
+  # Validate command for sidebar file opening.
+  proc file_okay {} {
+
+    return 1
+
+  }
+
+  ######################################################################
   # Called whenever the user enters a value
   proc lookup {value mode show_detail} {
 
@@ -673,6 +681,16 @@ namespace eval launcher {
               lassign $snippet name value
               lappend matches [register_temp ";$name" [list snippets::insert_snippet_into_current {} $value] $name $i "" launcher::snip_okay]
               incr i
+            }
+          }
+        }
+        ">" {
+          if {[llength [array names commands [get_command_name * launcher::file_okay 1]]] == 0} {
+            unregister * * 1
+            set i 0
+            foreach finfo [sidebar::get_shown_files] {
+              lassign $finfo name row
+              lappend matches [register_temp ">$name" [list sidebar::open_file $row] $name $i "" launcher::file_okay]
             }
           }
         }
