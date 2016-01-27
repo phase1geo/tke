ce701678276ba88126ff9c976248b3d564c28a81
Trevor Williams <phase1geo@users.sourceforge.net>
2016-01-26 22:27:32 -0600

    * Fixing issues with inCommentStringHelper bugs. * Improving performance of inCommentStringHelper.

--- tests/ctext.tcl
+++ tests/ctext.tcl
@@ -116,4 +116,174 @@ namespace eval ctext {
 
   }
 
+  # Verify isEscaped functionality
+  proc run_test4 {} {
+
+    set txt [initialize]
+
+    for {set i 1} {$i <= 8} {incr i} {
+      $txt insert end "\n[string repeat \\ $i]word"
+      if {[$txt get 2.$i end-1c] ne "word"} {
+        cleanup "Test index is not correct ([$txt get 2.$i end-1c])"
+      }
+      if {[expr $i % 2] != [ctext::isEscaped $txt 2.$i]} {
+        cleanup "escaped == $i not matching expected"
+      }
+      $txt delete 1.0 end
+    }
+
+    $txt insert end "\n\\ \\word"
+    if {![ctext::isEscaped $txt 2.3]} {
+      cleanup "split escapes are not being parsed properly"
+    }
+
+    cleanup
+
+  }
+
+  # Verify inDoubleQuote, inString and inCommentString functionality for double quotes
+  proc run_test5 {} {
+
+    set txt [initialize]
+    syntax::set_language $txt "C++"
+
+    $txt insert end "\nthis \"is a\" string"
+
+    if {[$txt tag ranges _dString] ne [list 2.5 2.11]} {
+      cleanup "tag does not match expected value"
+    }
+
+    foreach procedure [list inDoubleQuote inString inCommentString] {
+      foreach {index expect} [list 2.0 0 2.4 0 2.5 1 2.6 1 2.9 1 2.10 1 2.11 0] {
+        set range ""
+        if {[ctext::$procedure $txt $index range] != $expect} {
+          cleanup "$procedure: index $index did not match expected value ($expect)"
+        } elseif {!$expect && ($range ne "")} {
+          cleanup "$procedure: index $index returned a range when it should not have"
+        } elseif {$expect && ($range ne [list 2.5 2.11])} {
+          cleanup "$procedure: index $index returned a bad range ($range)"
+        }
+      }
+    }
+
+    cleanup
+
+  }
+
+  # Verify inSingleQuote, inString and inCommentString functionality for single quotes
+  proc run_test6 {} {
+
+    set txt [initialize]
+    syntax::set_language $txt "C++"
+
+    $txt insert end "\nthis 'is a' string"
+
+    if {[$txt tag ranges _sString] ne [list 2.5 2.11]} {
+      cleanup "tag does not match expected value"
+    }
+
+    foreach procedure [list inSingleQuote inString inCommentString] {
+      foreach {index expect} [list 2.0 0 2.4 0 2.5 1 2.6 1 2.9 1 2.10 1 2.11 0] {
+        set range ""
+        if {[ctext::$procedure $txt $index range] != $expect} {
+          cleanup "$procedure: index $index did not match expected value ($expect)"
+        } elseif {!$expect && ($range ne "")} {
+          cleanup "$procedure: index $index returned a range when it should not have"
+        } elseif {$expect && ($range ne [list 2.5 2.11])} {
+          cleanup "$procedure: index $index returned a bad range ($range)"
+        }
+      }
+    }
+
+    cleanup
+
+  }
+
+  # Verify inLineComment, inComment and inCommentString functionality for line comments
+  proc run_test7 {} {
+
+    set txt [initialize]
+    syntax::set_language $txt "C++"
+
+    $txt insert end "\nthis // is a line\ncomment"
+
+    if {[$txt tag ranges _lComment] ne [list 2.5 2.17]} {
+      cleanup "tag does not match expected value"
+    }
+
+    foreach procedure [list inLineComment inComment inCommentString] {
+      foreach {index expect} [list 2.0 0 2.4 0 2.5 1 2.6 1 2.7 1 2.16 1 2.end 0 3.0 0] {
+        set range ""
+        if {[ctext::$procedure $txt $index range] != $expect} {
+          cleanup "$procedure: index $index did not match expected value ($expect)"
+        } elseif {!$expect && ($range ne "")} {
+          cleanup "$procedure: index $index returned a range when it should not have"
+        } elseif {$expect && ($range ne [list 2.5 2.17])} {
+          cleanup "$procedure: index $index returned a bad range ($range)"
+        }
+      }
+    }
+
+    cleanup
+
+  }
+
+  # Verify inBlockComment, inComment and inCommentString functionality for block comments
+  proc run_test8 {} {
+
+    set txt [initialize]
+    syntax::set_language $txt "C++"
+
+    $txt insert end "\nthis /* is a block */ comment"
+
+    if {[$txt tag ranges _cComment] ne [list 2.5 2.21]} {
+      cleanup "tag does not match expected value ($range)"
+    }
+
+    foreach procedure [list inBlockComment inComment inCommentString] {
+      foreach {index expect} [list 2.0 0 2.4 0 2.5 1 2.6 1 2.7 1 2.18 1 2.19 1 2.20 1 2.21 0] {
+        set range ""
+        if {[ctext::$procedure $txt $index range] != $expect} {
+          cleanup "$procedure: index $index did not match expected value ($expect)"
+        } elseif {!$expect && ($range ne "")} {
+          cleanup "$procedure: index $index returned a range when it should not have"
+        } elseif {$expect && ($range ne [list 2.5 2.21])} {
+          cleanup "$procedure: index $index returned a bad range ($range)"
+        }
+      }
+    }
+
+    cleanup
+
+  }
+
+  # Verify inTripleQuote, inString and inCommentString functionality for triple quote
+  proc run_test9 {} {
+
+    set txt [initialize]
+    syntax::set_language $txt "Python"
+
+    $txt insert end "\nthis \"\"\"is a triple\"\"\" quote"
+
+    if {[$txt tag ranges _tString] ne [list 2.5 2.22]} {
+      cleanup "tag does not match expected value ($range)"
+    }
+
+    foreach procedure [list inTripleQuote inString inCommentString] {
+      foreach {index expect} [list 2.0 0 2.4 0 2.5 1 2.6 1 2.7 1 2.8 1 2.18 1 2.19 1 2.20 1 2.21 1 2.22 0] {
+        set range ""
+        if {[ctext::$procedure $txt $index range] != $expect} {
+          cleanup "$procedure: index $index did not match expected value ($expect)"
+        } elseif {!$expect && ($range ne "")} {
+          cleanup "$procedure: index $index returned a range when it should not have"
+        } elseif {$expect && ($range ne [list 2.5 2.22])} {
+          cleanup "$procedure: index $index returned a bad range ($range)"
+        }
+      }
+    }
+
+    cleanup
+
+  }
+
 }
