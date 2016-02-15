99dadfa028a7b9058496549868400f2f47bd8a7a
Trevor Williams <phase1geo@users.sourceforge.net>
2016-02-15 12:29:28 -0600

    * Checkpointing work on improved switch statement indentation (work in progress). * Adding Erlang syntax.

--- lib/ctext/ctext.tcl
+++ lib/ctext/ctext.tcl
@@ -2848,7 +2848,7 @@ proc ctext::doReindent {twin start end} {
 
   # Perform reindent match
   set i 0
-  foreach res [$twin search -count lengths -regexp -nolinestop -all -- {switch.+?\{.*?case.*?:} 1.0 $end] {
+  foreach res [$twin search -count lengths -regexp -nolinestop -all -- {switch.+?\{.*?case} 1.0 $end] {
     puts "HERE"
     $twin tag add _reindent $res [$twin index "$res + [lindex $lengths $i] chars"]
     incr i
