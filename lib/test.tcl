df6047277da80e03a8ba21e7e5a419ca69cea420
Trevor Williams <phase1geo@gmail.com>
2016-02-22 17:38:30 -0600

    * Emmet updates.

--- lib/test.tcl
+++ lib/test.tcl
@@ -15,6 +15,12 @@ foreach str [list "nav>ul>li" \
                   "p\[title=\"Hello World\"\]" \
                   "td\[rowspan=2 colspan=3 title\]" \
                   "p>{Click }+a{here}+{ to continue}" \
+                  "link" \
+                  "link\[rel=prefetch title=\"Hello world\"\]" \
+                  ".wrap>.content" \
+                  "em>.info" \
+                  "ul>.item" \
+                  "table>#row>\[colspan=2\]" \
                   ] {
   puts "str: $str\n"
   puts [parse_emmet $str]
