67ff2a148f0b659919e2e0decf1f3c763f8d74ae
Trevor Williams <phase1geo@users.sourceforge.net>
2016-03-19 00:02:02 -0500

    * Adding support for several Emmet CSS preferences.

--- lib/emmet.tcl
+++ lib/emmet.tcl
@@ -23,11 +23,12 @@
 ######################################################################
 
 source [file join $::tke_dir lib emmet_parser.tcl]
+source [file join $::tke_dir lib emmet_css.tcl]
 
 namespace eval emmet {
 
   source [file join $::tke_dir lib ns.tcl]
-  
+
   array set data {
     tag      {(.*)(<\/?[\w:-]+(?:\s+[\w:-]+(?:\s*=\s*(?:(?:".*?")|(?:'.*?')|[^>\s]+))?)*\s*(\/?)>)}
     brackets {(.*?)(\[.*?\]|\{.*?\})}
@@ -38,24 +39,24 @@ namespace eval emmet {
   # Returns a three element list containing the snippet text, starting and ending
   # position of that text.
   proc get_snippet_text_pos {tid} {
-    
+
     variable data
-    
+
     # Get the current text widget
     set txt [[ns gui]::current_txt $tid]
-    
+
     # Get the current line and column numbers
     lassign [split [$txt index insert] .] line endcol
 
     # Get the current line
     set str [$txt get "insert linestart" insert]
-    
+
     # Get the prespace of the current line
     regexp {^([ \t]*)} $str -> prespace
-    
+
     # If we have a tag, ignore all text prior to it
     set startcol [expr {[regexp $data(tag) $str match] ? [string length $match] : 0}]
-    
+
     # Gather the positions of any square or curly brackets in the left-over area
     foreach key [list brackets space] {
       set pos($key) [list]
@@ -65,14 +66,14 @@ namespace eval emmet {
         set col [lindex $pos($key) end]
       }
     }
-    
+
     # See if there is a space which does not exist within a square or curly brace
     foreach {endpos startpos} [lreverse $pos(space)] {
       if {[expr [lsearch [lsort -integer [concat $pos(brackets) $endpos]] $endpos] % 2] == 0} {
         return [list [string range $str $endpos end] $line.$endpos $line.$endcol $prespace]
       }
     }
-    
+
     return [list [string range $str $startcol end] $line.$startcol $line.$endcol $prespace]
 
   }
@@ -82,29 +83,29 @@ namespace eval emmet {
   # Returns a three element list containing the generated code, the
   # starting index of the snippet and the ending index of the snippet.
   proc expand_abbreviation {tid} {
-    
+
     set txt [[ns gui]::current_txt $tid]
-    
+
     # If the current language is CSS, translate the abbreviation as such
     if {[[ns syntax]::get_language $txt] eq "CSS"} {
-      
+
       # Get the abbreviation text, translate it and insert it back into the text
       if {[regexp {(\S+)$} [$txt get "insert linestart" insert] -> abbr]} {
         if {![catch { [ns emmet_css]::parse $abbr } str]} {
           [ns snippets]::insert_snippet_into_current $tid $str "insert-[string length $abbr]c" insert
         }
       }
-      
+
     } else {
 
       # Find the snippet text
       lassign [get_snippet_text_pos $tid] str startpos endpos prespace
-    
+
       # Parse the snippet and if no error, insert the resulting string
       if {![catch { ::parse_emmet $str $prespace } str]} {
         [ns snippets]::insert_snippet_into_current $tid $str $startpos $endpos
       }
-      
+
     }
 
   }
