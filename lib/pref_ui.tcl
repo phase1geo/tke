cfea3719cc4e6ec64ce52f8604fdd2f97910b7cb
Trevor Williams <phase1geo@users.sourceforge.net>
2016-05-17 20:23:58 -0500

    * Checkpointing preference UI work.

--- lib/pref_ui.tcl
+++ lib/pref_ui.tcl
@@ -61,11 +61,24 @@ namespace eval pref_ui {
         $widgets(bar) insert end [string totitle $pane]
         create_$pane [set widgets($pane) [ttk::frame $widgets(frame).$pane]]
       }
-      
+
+      $widgets(bar) selection set 0
+      show_panel general
+
+      trace add variable [[ns preferences]::ref] write [list pref_ui::handle_prefs_change]
+
     }
 
-    $widgets(bar) selection set 0
-    show_panel general
+  }
+
+  ######################################################################
+  # Handles any changes to the preference array.
+  proc handle_prefs_change {name1 name2 op} {
+
+    if {[winfo exists .prefwin]} {
+      # [ns preferences]::save_prefs
+      puts "Saving prefs"
+    }
 
   }
 
@@ -99,64 +112,120 @@ namespace eval pref_ui {
     pack [ttk::checkbutton $a.acwd -text "Automatically set the current working directory to the current tabs directory" -variable $acwd] -fill x
 
     $w.nb add [set b [ttk::frame $w.nb.b]] -text "Variables"
-    
+
     ttk::frame $b.f
     set widgets(var_table) [tablelist::tablelist $b.f.tl -columns {0 {Variable} 0 {Value}} \
-      -stretch all \
+      -stretch all -editselectedonly 1 -exportselection 1 \
+      -editendcommand [list pref_ui::var_edit_end_command] \
       -xscrollcommand [list $b.f.hb set] -yscrollcommand [list $b.f.vb set]]
     ttk::scrollbar $b.f.vb -orient vertical   -command [list $b.f.tl yview]
     ttk::scrollbar $b.f.hb -orient horizontal -command [list $b.f.tl xview]
-    
-    bind $widgets(var_table) <<TablelistSelect>>
-    
+
+    $widgets(var_table) columnconfigure 0 -name var -editable 1 -stretchable 1
+    $widgets(var_table) columnconfigure 1 -name val -editable 1 -stretchable 1
+
+    bind $widgets(var_table) <<TablelistSelect>> [list pref_ui::handle_var_select]
+
     grid rowconfigure    $b.f 0 -weight 1
     grid columnconfigure $b.f 0 -weight 1
     grid $b.f.tl -row 0 -column 0 -sticky news
     grid $b.f.vb -row 0 -column 1 -sticky ns
     grid $b.f.hb -row 1 -column 0 -sticky ew
-    
+
     ttk::frame $b.bf
-    set widgets(var_add) [ttk::button $b.bf.add -text "Add"    -command [list pref_ui::add_variable]]
-    set widgets(var_del) [ttk::button $b.bf.del -text "Delete" -command [list pref_ui::del_variable]] -state disabled
-    
+    set widgets(var_add) [ttk::button $b.bf.add -style BButton -text "Add"    -command [list pref_ui::add_variable]]
+    set widgets(var_del) [ttk::button $b.bf.del -style BButton -text "Delete" -command [list pref_ui::del_variable] -state disabled]
+
     pack $b.bf.add -side left -padx 2 -pady 2
     pack $b.bf.del -side left -padx 2 -pady 2
-    
+
     pack $b.f  -fill both -expand yes
     pack $b.bf -fill x
-    
+
+    # Populate the variable table
+    foreach row [[ns preferences]::get General/Variables] {
+      $widgets(var_table) insert end $row
+    }
+
     $w.nb add [set c [ttk::frame $w.nb.c]] -text "Language Overrides"
 
   }
-  
+
+  ######################################################################
+  # When a row is selected, set the delete button state to normal.
+  proc handle_var_select {} {
+
+    variable widgets
+
+    # Enable the delete button
+    if {[$widgets(var_table) curselection] eq ""} {
+      $widgets(var_del) configure -state disabled
+    } else {
+      $widgets(var_del) configure -state normal
+    }
+
+  }
+
   ######################################################################
   # Adds a variable to the end of the variable table.
   proc add_variable {} {
-    
+
     variable widgets
-    
+
     # Add the new variable line
     set row [$widgets(var_table) insert end [list "" ""]]
-    
+
     # Make the first entry to be editable
-    $widgets(var_table) editcell $row
-    
+    $widgets(var_table) editcell $row,var
+
   }
-  
+
   ######################################################################
   # Deletes the currently selected variable from the variable table.
   proc del_variable {} {
-    
+
     variable widgets
-    
+
     set selected [$widgets(var_table) curselection]
-    
+
     # Delete the row
     $widgets(var_table) delete $selected
-    
+
     # Disable the delete button
     $widgets(var_del) configure -state disabled
-    
+
+    # Update the General/Variable array value
+    gather_var_table
+
+  }
+
+  ######################################################################
+  # Called after the user has edited the variable table cell.
+  proc var_edit_end_command {tbl row col value} {
+
+    if {([$tbl cellcget $row,var -text] ne "") && ([$tbl cellcget $row,val -text] ne "")} {
+      after 1 [list pref_ui::gather_var_table]
+    }
+
+    return $value
+
+  }
+
+  ######################################################################
+  # Gather the variable table values from the table and update the
+  # preferences array.
+  proc gather_var_table {} {
+
+    variable widgets
+
+    set values [list]
+
+    for {set i 0} {$i < [$widgets(var_table) size]} {incr i} {
+      lappend values [$widgets(var_table) get $i]
+    }
+
+    set [[ns preferences]::ref General/Variables] $values
+
   }
 
   ######################################################################
