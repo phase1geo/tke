f87e95a8c929eaa9c1c1543776cd947f218a23ce
Trevor Williams <phase1geo@users.sourceforge.net>
2016-09-09 10:23:56 -0500

    * Upgrading tablelist widget to version 5.16

--- lib/tke.tcl
+++ lib/tke.tcl
@@ -55,7 +55,7 @@ proc tke_development {} {
 wm withdraw .
 
 set auto_path [list [file join $tke_dir lib ctext] \
-                    [file join $tke_dir lib tablelist5.14] \
+                    [file join $tke_dir lib tablelist5.16] \
                     [file join $tke_dir lib ptwidgets1.2] \
                     {*}$auto_path]
 
@@ -65,7 +65,7 @@ if {$tcl_platform(platform) eq "windows"} {
   package require Tclx
 }
 package require -exact ctext 5.0
-package require -exact tablelist 5.14
+package require -exact tablelist 5.16
 package require tooltip
 package require msgcat
 package require tokenentry
