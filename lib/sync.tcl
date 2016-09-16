2d3e928bd264aed094f9db04435cfb32c08d7067
Trevor Williams <phase1geo@users.sourceforge.net>
2016-09-16 00:17:26 -0500

    * Checkpointing work on sync namespace.

--- lib/sync.tcl
+++ lib/sync.tcl
@@ -26,10 +26,7 @@ namespace eval sync {
 
   source [file join $::tke_dir lib ns.tcl]
 
-  variable last_sync_dir   "none"
-  variable last_sync_items "none"
-  variable sync_local      [file join $::tke_home sync_dir]
-  variable use_sync        [file join $::tke_home use_sync]
+  variable sync_local [file join $::tke_home sync_dir]
 
   trace variable [ns preferences]::prefs(General/SyncDirectory) w [list sync::sync_changed]
   trace variable [ns preferences]::prefs(General/SyncItems)     w [list sync::sync_changed]
@@ -38,65 +35,74 @@ namespace eval sync {
   # Called whenever the General/SyncDirectory value changes.
   proc sync_changed {name1 name2 op} {
 
-    variable last_sync_dir
-    variable last_sync_items
     variable sync_local
-    variable use_sync
 
     # Get the new sync directory value
     set sync_dir   [[ns preferences]::get General/SyncDirectory]
     set sync_items [[ns preferences]::get General/SyncItems]
 
     if {$sync_dir ne ""} {
-      create_sync_dir $sync_dir
-      if {[lsearch $sync_items prefs] != -1} {
-        if {[catch { open $use_sync w } rc]} {
-          close $rc
-        }
-      } else {
-        file delete -force $use_sync
-      }
-    } elseif {[file exists $sync_local]} {
-      file delete -force [file link $sync_local] $use_sync
+      create_sync_dir $sync_dir $sync_items
+    } else {
+      remove_sync_dir $sync_dir
     }
 
-    # Save the values
-    set last_sync_dir   $sync_dir
-    set last_sync_items $sync_items
+    # We need to restart TKE to have the change take full effect
+    # TBD
 
   }
 
   ######################################################################
   # Create the sync directory and copy the current items to it and create
   # symlinks, if necessary.
-  proc create_sync_dir {sync_dir} {
+  proc create_sync_dir {sync_dir sync_items} {
 
     variable sync_local
 
     # Create the synchronization directory
     file mkdir $sync_dir
 
-    # Remove the sync directory if it exists and points at a different sync
-    # directory and create the new symlink.
-    if {[file exists $sync_local]} {
-      if {[file link $sync_local] ne $sync_dir} {
-        file delete -force $sync_local
-        file link -symbolic $sync_local $sync_dir
-      } else {
-        return
+    # Copy the relevant files to the sync directory
+    foreach {type nspace name} [get_sync_items] {
+      foreach item [[ns $nspace]::get_sync_items] {
+        set home_item [file join $::tke_home $item]
+        set sync_item [file join $sync_dir $item]
+        if {[file exists $home_item] && ![file exists $sync_item]]} {
+          file copy -force $home_item $sync_dir
+        }
       }
-    } else {
+    }
+
+    # Remove the sync directory link, if it exists
+    file delete -force $sync_local
+
+    # Create the sync_local link if we need to get our preferences from the
+    # remote directory
+    if {[lsearch $sync_items prefs] != -1} {
       file link -symbolic $sync_local $sync_dir
     }
+
+  }
+
+  ######################################################################
+  # Copies the items from the sync directory to the local home directory
+  # and removes the symlink to the sync directory (if it exists).
+  proc remove_sync_dir {sync_dir} {
+
+    variable sync_local
 
     # Copy the relevant files to the sync directory
     foreach {type nspace name} [get_sync_items] {
       foreach item [[ns $nspace]::get_sync_items] {
-        if {[file exists $item]} {
-          file copy -force [file join $::tke_home $item] $sync_dir
+        set sync_item [file join $sync_dir $item]
+        if {[file exists $sync_item]} {
+          file copy -force $sync_item $::tke_home
         }
       }
     }
+
+    # Remove the sync directory link, if it exists
+    file delete -force $sync_local
 
   }
 
@@ -122,15 +128,18 @@ namespace eval sync {
     variable sync_local
     variable use_sync
 
-    # Determine if the sync directory is valid and connected to
-    set sync_exists [expr {[file exists $sync_local] && [file exists [file link $sync_local]]}]
-
+    # We need to special treat the preferences type special as we might not know the
+    # values of sync_dir and sync_items.
     if {$type eq "prefs"} {
-      return [expr {($sync_exists && [file exists $use_sync]) ? $sync_local : $::tke_home}]
-    } elseif {[lsearch [[ns preferences]::get General/SyncItems] $type] != -1} {
+      set sync_exists [expr {[file exists $sync_local] && [file exists [file link $sync_local]]}]
       return [expr {$sync_exists ? $sync_local : $::tke_home}]
+
+    # Otherwise, check the state of the type in the sync_items list and look for the
+    # remote or local files based on that value.
     } else {
-      return $::tke_home
+      set sync_dir   [[ns preferences]::get General/SyncDirectory]
+      set sync_items [[ns preferences]::get General/SyncItems]
+      return [expr {([file exists $sync_dir] && ([lsearch $sync_items $type] != -1)) ? $sync_dir : $::tke_home}]
     }
 
   }
--- lib/version.tcl
+++ lib/version.tcl
@@ -1,4 +1,4 @@
 set version_major "2"
 set version_minor "6"
 set version_point "4"
-set version_hgid  "2287"
+set version_hgid  "2288"
