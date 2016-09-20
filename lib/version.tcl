d421771ff1133e42918bfe02da7dce3ae7c5a23d
Trevor Williams <phase1geo@users.sourceforge.net>
2016-09-20 00:01:41 -0500

    ChangeLog for devel-2.6.6 release

--- lib/version.tcl
+++ lib/version.tcl
@@ -1,4 +1,4 @@
 set version_major "2"
 set version_minor "6"
-set version_point "5"
-set version_hgid  "2300"
+set version_point "6"
+set version_hgid  "2301"
--- specl_version.tcl
+++ specl_version.tcl
@@ -1,5 +1,5 @@
 set specl::appname      "tke"
-set specl::version      "2.6.5"
-set specl::release      "269"
+set specl::version      "2.6.6"
+set specl::release      "270"
 set specl::rss_url      "http://tke.sourceforge.net/releases"
 set specl::download_url "http://tke.sourceforge.net/releases"
