d85119eeeebe5152eba75af9d3b24b8822f330ff
Trevor Williams <phase1geo@users.sourceforge.net>
2016-09-28 08:51:01 -0500

    * Checkpointing startup work.

--- lib/foo.tcl
+++ lib/foo.tcl
@@ -1,7 +1,9 @@
-set tke_dir [file normalize [file join [pwd] ..]]
+set tke_dir  [file normalize [file join [pwd] ..]]
+set tke_home [file normalize [file join ~ .tke]]
 
 source startup.tcl
 source utils.tcl
+source sync.tcl
 
 lassign [startup::create] type dir
 
