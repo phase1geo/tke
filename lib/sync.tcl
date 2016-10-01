72dbb06bb2e1c54fe84f3f931bc7826a0bc9e1fc
Trevor Williams <phase1geo@users.sourceforge.net>
2016-10-01 12:38:32 -0500

    * Fixing the sidebar open indicator and cursor color in the Monokai theme. * Changing internal names of sync to share.

--- lib/sync.tcl
+++ lib/sync.tcl
@@ -16,17 +16,16 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 ######################################################################
-# Name:    sync.tcl
+# Name:    share.tcl
 # Author:  Trevor Williams  (phase1geo@gmail.com)
 # Date:    9/14/2016
-# Brief:   Namespace that handles settings sync.
+# Brief:   Namespace that handles settings sharing.
 ######################################################################
 
-namespace eval sync {
+namespace eval share {
 
   source [file join $::tke_dir lib ns.tcl]
 
-  variable sync_local     [file join $::tke_home sync_dir]
   variable last_directory ""
   variable last_items     [list]
 
@@ -35,8 +34,8 @@ namespace eval sync {
   array set items   {}
 
   ######################################################################
-  # Returns the list of sync items.
-  proc get_sync_items {} {
+  # Returns the list of sharing items.
+  proc get_share_items {} {
 
     return [list \
       emmet     emmet       [msgcat::mc "Emmet"] \
@@ -53,23 +52,23 @@ namespace eval sync {
   }
 
   ######################################################################
-  # Called by the whenever sync items are changed.
-  proc save_changes {sync_dir sync_items} {
+  # Called by the whenever sharing items are changed.
+  proc save_changes {share_dir share_items} {
 
     variable data
     variable last_directory
     variable last_items
 
     # Save the last directory
-    set last_directory $data(SyncDirectory)
-    set last_items     $data(SyncItems)
+    set last_directory $data(ShareDirectory)
+    set last_items     $data(ShareItems)
 
     # Save the changes
-    set data(SyncDirectory) $sync_dir
-    set data(SyncItems)     $sync_items
+    set data(ShareDirectory) $share_dir
+    set data(ShareItems)     $share_items
 
-    # Indicate that the sync directories have changed
-    sync_changed
+    # Indicate that the share information have changed
+    share_changed
 
     # If the directory changed but was previously pointing at a remote directory,
     # transfer all of the information that was being stored in the remote directory
@@ -78,10 +77,10 @@ namespace eval sync {
       file_transfer $last_directory $::tke_home $last_items
     }
 
-    # If we are using a remote directory, create the directory and sync the files
+    # If we are using a remote directory, create the directory and share the files
     # to that directory.
-    if {$sync_dir ne ""} {
-      create_sync_dir $data(SyncDirectory) $data(SyncItems)
+    if {$share_dir ne ""} {
+      create_share_dir $data(ShareDirectory) $data(ShareItems)
     }
 
     # Write the file contents
@@ -90,56 +89,56 @@ namespace eval sync {
   }
 
   ######################################################################
-  # Update all namespaces with the updated sync information.
-  proc sync_changed {} {
+  # Update all namespaces with the updated sharing information.
+  proc share_changed {} {
 
     variable data
 
-    # Indicate the new directory to all sync items
-    if {$data(SyncDirectory) ne ""} {
-      foreach {type nspace name} [get_sync_items] {
-        [ns $nspace]::sync_changed [expr {([lsearch $data(SyncItems) $type] != -1) ? $data(SyncDirectory) : $::tke_home}]
+    # Indicate the new directory to all sharing items
+    if {$data(ShareDirectory) ne ""} {
+      foreach {type nspace name} [get_share_items] {
+        [ns $nspace]::share_changed [expr {([lsearch $data(ShareItems) $type] != -1) ? $data(ShareDirectory) : $::tke_home}]
       }
     } else {
-      foreach {type nspace name} [get_sync_items] {
-        [ns $nspace]::sync_changed $::tke_home
+      foreach {type nspace name} [get_share_items] {
+        [ns $nspace]::share_changed $::tke_home
       }
     }
 
   }
 
   ######################################################################
-  # Create the sync directory and copy the current items to it and create
+  # Create the share directory and copy the current items to it and create
   # symlinks, if necessary.
-  proc create_sync_dir {sync_dir sync_items} {
+  proc create_share_dir {share_dir share_items} {
 
     variable last_items
 
-    # Create the synchronization directory
-    file mkdir $sync_dir
+    # Create the sharing directory
+    file mkdir $share_dir
 
-    foreach {type nspace name} [get_sync_items] {
+    foreach {type nspace name} [get_share_items] {
 
-      # Copy the relevant files to the sync directory
-      if {[lsearch $sync_items $type] != -1} {
-        foreach item [[ns $nspace]::get_sync_items $::tke_home] {
-          set home_item [file join $::tke_home $item]
-          set sync_item [file join $sync_dir $item]
-          if {[file exists $home_item] && ![file exists $sync_item]} {
-            file copy -force $home_item $sync_dir
+      # Copy the relevant files to the share directory
+      if {[lsearch $share_items $type] != -1} {
+        foreach item [[ns $nspace]::get_share_items $::tke_home] {
+          set home_item  [file join $::tke_home $item]
+          set share_item [file join $share_dir $item]
+          if {[file exists $home_item] && ![file exists $share_item]} {
+            file copy -force $home_item $share_dir
           }
         }
 
-      # Otherwise, copy relevant items to home directory if we used to get the items from the sync directory
-      } elseif {([lsearch $sync_items $type] == -1) && ([lsearch $last_items $type] != -1)} {
-        foreach item [[ns $nspace]::get_sync_items $sync_dir] {
-          set home_item [file join $::tke_home $item]
-          set sync_item [file join $sync_dir $item]
-          if {[file exists $sync_item]} {
+      # Otherwise, copy relevant items to home directory if we used to get the items from the share directory
+      } elseif {([lsearch $share_items $type] == -1) && ([lsearch $last_items $type] != -1)} {
+        foreach item [[ns $nspace]::get_share_items $share_dir] {
+          set home_item  [file join $::tke_home $item]
+          set share_item [file join $share_dir $item]
+          if {[file exists $share_item]} {
             if {[file exists $home_item] && [file isdirectory $home_item]} {
               file delete -force $tname
             }
-            file copy -force $sync_item $::tke_home
+            file copy -force $share_item $::tke_home
           }
         }
       }
@@ -151,17 +150,17 @@ namespace eval sync {
   ######################################################################
   # Performs a file transfer, removing any items that are in the target
   # directory that will be moved.  This is used when importing/exporting
-  # settings data (use the create_sync_dir) method to perform a file sync.
-  proc file_transfer {from_dir to_dir sync_items} {
+  # settings data (use the create_share_dir) method to perform a file share.
+  proc file_transfer {from_dir to_dir share_items} {
 
     variable data
 
     # Get the list of files/directories to transfer based on the items
-    foreach {type nspace name} [get_sync_items] {
-      set fdir [expr {(($from_dir eq "") || (($from_dir eq $data(SyncDirectory)) && ([lsearch $data(SyncItems) $type] == -1))) ? $::tke_home : $from_dir}]
-      set tdir [expr {(($to_dir   eq "") || (($to_dir   eq $data(SyncDirectory)) && ([lsearch $data(SyncItems) $type] == -1))) ? $::tke_home : $to_dir}]
-      if {[lsearch $sync_items $type] != -1} {
-        foreach item [[ns $nspace]::get_sync_items $fdir] {
+    foreach {type nspace name} [get_share_items] {
+      set fdir [expr {(($from_dir eq "") || (($from_dir eq $data(ShareDirectory)) && ([lsearch $data(ShareItems) $type] == -1))) ? $::tke_home : $from_dir}]
+      set tdir [expr {(($to_dir   eq "") || (($to_dir   eq $data(ShareDirectory)) && ([lsearch $data(ShareItems) $type] == -1))) ? $::tke_home : $to_dir}]
+      if {[lsearch $share_items $type] != -1} {
+        foreach item [[ns $nspace]::get_share_items $fdir] {
           if {[file exists [set fname [file join $fdir $item]]]} {
             set tname [file join $tdir $item]
             if {[file exists $tname] && [file isdirectory $tname]} {
@@ -176,18 +175,18 @@ namespace eval sync {
   }
 
   ######################################################################
-  # Returns the sync information.
-  proc get_sync_info {} {
+  # Returns the sharing information.
+  proc get_share_info {} {
 
     variable data
 
-    # Gather the sync items
+    # Gather the share items
     set items [list]
-    foreach {type nspace name} [get_sync_items] {
-      lappend items $type [expr [lsearch $data(SyncItems) $type] != -1]
+    foreach {type nspace name} [get_share_items] {
+      lappend items $type [expr [lsearch $data(ShareItems) $type] != -1]
     }
 
-    return [list $data(SyncDirectory) $items]
+    return [list $data(ShareDirectory) $items]
 
   }
 
@@ -199,57 +198,57 @@ namespace eval sync {
     variable data
     variable items
 
-    toplevel     .syncwin
-    wm title     .syncwin [msgcat::mc "Export Settings Data"]
-    wm resizable .syncwin 0 0
-    wm transient .syncwin .
+    toplevel     .sharewin
+    wm title     .sharewin [msgcat::mc "Export Settings Data"]
+    wm resizable .sharewin 0 0
+    wm transient .sharewin .
 
-    ttk::frame  .syncwin.f
-    ttk::label  .syncwin.f.l -text [format "%s: " [msgcat::mc "Directory"]]
-    set widgets(directory) [ttk::entry .syncwin.f.e -width 40 -state readonly]
-    ttk::button .syncwin.f.b -style BButton -text [format "%s..." [msgcat::mc "Browse"]] -command [list sync::browse_directory]
+    ttk::frame  .sharewin.f
+    ttk::label  .sharewin.f.l -text [format "%s: " [msgcat::mc "Directory"]]
+    set widgets(directory) [ttk::entry .sharewin.f.e -width 40 -state readonly]
+    ttk::button .sharewin.f.b -style BButton -text [format "%s..." [msgcat::mc "Browse"]] -command [list share::browse_directory]
 
-    pack .syncwin.f.l -side left  -padx 2 -pady 2
-    pack .syncwin.f.e -side left  -padx 2 -pady 2 -fill x -expand yes
-    pack .syncwin.f.b -side right -padx 2 -pady 2
+    pack .sharewin.f.l -side left  -padx 2 -pady 2
+    pack .sharewin.f.e -side left  -padx 2 -pady 2 -fill x -expand yes
+    pack .sharewin.f.b -side right -padx 2 -pady 2
 
-    ttk::frame      .syncwin.lf
-    ttk::labelframe .syncwin.lf.f -text [msgcat::mc "Settings to export"]
+    ttk::frame      .sharewin.lf
+    ttk::labelframe .sharewin.lf.f -text [msgcat::mc "Settings to export"]
     set i       0
     set columns 3
-    foreach {type nspace name} [get_sync_items] {
+    foreach {type nspace name} [get_share_items] {
       set items($type) 1
-      grid [ttk::checkbutton .syncwin.lf.f.$type -text $name -variable sync::items($type) -command [list sync::handle_do_state]] -row [expr $i % $columns] -column [expr $i / $columns] -sticky news -padx 2 -pady 2
+      grid [ttk::checkbutton .sharewin.lf.f.$type -text $name -variable share::items($type) -command [list share::handle_do_state]] -row [expr $i % $columns] -column [expr $i / $columns] -sticky news -padx 2 -pady 2
       incr i
     }
 
-    pack .syncwin.lf.f -side left -padx 20 -pady 2
+    pack .sharewin.lf.f -side left -padx 20 -pady 2
 
-    ttk::frame  .syncwin.bf
-    set widgets(do) [ttk::button .syncwin.bf.do -style BButton -text [msgcat::mc "Export"] -width 6 -command [list sync::do_export]]
-    ttk::button .syncwin.bf.cancel -style BButton -text [msgcat::mc "Cancel"] -width 6 -command [list sync::do_cancel]
+    ttk::frame  .sharewin.bf
+    set widgets(do) [ttk::button .sharewin.bf.do -style BButton -text [msgcat::mc "Export"] -width 6 -command [list share::do_export]]
+    ttk::button .sharewin.bf.cancel -style BButton -text [msgcat::mc "Cancel"] -width 6 -command [list share::do_cancel]
 
-    pack .syncwin.bf.cancel -side right -padx 2 -pady 2
-    pack .syncwin.bf.do     -side right -padx 2 -pady 2
+    pack .sharewin.bf.cancel -side right -padx 2 -pady 2
+    pack .sharewin.bf.do     -side right -padx 2 -pady 2
 
-    pack .syncwin.f  -fill x
-    pack .syncwin.lf -fill both -expand yes
-    pack .syncwin.bf -fill x
+    pack .sharewin.f  -fill x
+    pack .sharewin.lf -fill both -expand yes
+    pack .sharewin.bf -fill x
 
     # Handle the state of the do button
     handle_do_state
 
     # Grab the focus
-    ::tk::SetFocusGrab .syncwin .syncwin.f.b
+    ::tk::SetFocusGrab .sharewin .sharewin.f.b
 
     # Center the window
-    ::tk::PlaceWindow .syncwin widget .
+    ::tk::PlaceWindow .sharewin widget .
 
     # Wait for the window to close
-    tkwait window .syncwin
+    tkwait window .sharewin
 
     # Restore the grab and focus
-    ::tk::RestoreFocusGrab .syncwin .syncwin.f.b
+    ::tk::RestoreFocusGrab .sharewin .sharewin.f.b
 
   }
 
@@ -262,7 +261,7 @@ namespace eval sync {
     variable widgets
 
     # Get the directory from the user
-    if {[set dir [tk_chooseDirectory -parent .syncwin -initialdir [pwd]]] ne ""} {
+    if {[set dir [tk_chooseDirectory -parent .sharewin -initialdir [pwd]]] ne ""} {
 
       # Insert the directory
       $widgets(directory) configure -state normal
@@ -289,7 +288,7 @@ namespace eval sync {
     $widgets(do) configure -state disabled
 
     if {[$widgets(directory) get] ne ""} {
-      foreach {type nspace name} [get_sync_items] {
+      foreach {type nspace name} [get_share_items] {
         if {$items($type)} {
           $widgets(do) configure -state normal
           return
@@ -309,58 +308,58 @@ namespace eval sync {
 
     # Copy the relevant files to transfer
     set item_list [list]
-    foreach {type nspace name} [get_sync_items] {
+    foreach {type nspace name} [get_share_items] {
       if {$items($type)} {
         lappend item_list $type
       }
     }
 
-    # Get the sync directory
+    # Get the share directory
     set to_dir [$widgets(directory) get]
 
     # Perform the file transfer
-    if {$data(SyncDirectory) ne $to_dir} {
-      file_transfer $data(SyncDirectory) $to_dir $item_list
+    if {$data(ShareDirectory) ne $to_dir} {
+      file_transfer $data(ShareDirectory) $to_dir $item_list
     }
 
-    # Close the sync window
-    destroy .syncwin
+    # Close the share window
+    destroy .sharewin
 
   }
 
   ######################################################################
-  # Cancels the sync window.
+  # Cancels the share window.
   proc do_cancel {} {
 
-    # Close the sync window
-    destroy .syncwin
+    # Close the share window
+    destroy .sharewin
 
   }
 
   ######################################################################
-  # Called on tool startup.  If the sync file does not exist in the home
-  # directory, call the sync wizard to help the user setup a valid sync
+  # Called on tool startup.  If the share file does not exist in the home
+  # directory, call the share wizard to help the user setup a valid share
   # file.
   proc initialize {} {
 
     variable data
 
-    if {[file exists [file join $::tke_home sync.tkedat]]} {
+    if {[file exists [file join $::tke_home share.tkedat]]} {
       load_file
     } elseif {[llength [glob -nocomplain -directory $::tke_home *]] > 0} {
-      set data(SyncDirectory) ""
-      set data(SyncItems)     [list]
+      set data(ShareDirectory) ""
+      set data(ShareItems)     [list]
       write_file
     } else {
-      import_sync_wizard
+      import_share_wizard
     }
 
   }
 
   ######################################################################
-  # Displays the import/sync wizard which will be displayed if TKE is
-  # started and a sync.tkedat file is not found in the user's home directory.
-  proc import_sync_wizard {} {
+  # Displays the import/share wizard which will be displayed if TKE is
+  # started and a share.tkedat file is not found in the user's home directory.
+  proc import_share_wizard {} {
 
     variable data
     variable items
@@ -370,82 +369,82 @@ namespace eval sync {
 
     array set items $item_list
 
-    # Setup the sync directory
-    set data(SyncDirectory) [expr {($action eq "share") ? $dirname : ""}]
+    # Setup the share directory
+    set data(ShareDirectory) [expr {($action eq "share") ? $dirname : ""}]
 
-    # Setup the sync items list
-    set data(SyncItems) [list]
+    # Setup the share items list
+    set data(ShareItems) [list]
 
     if {$action ne "local"} {
-      foreach {type nspace name} [get_sync_items] {
+      foreach {type nspace name} [get_share_items] {
         if {$items($type)} {
-          lappend data(SyncItems) $type
+          lappend data(ShareItems) $type
         }
       }
     }
 
-    # Indicate that the sync directories have changed
-    sync_changed
+    # Indicate that the share directory have changed
+    share_changed
 
     # Perform the action
     switch $action {
-      copy  { file_transfer $dirname $::tke_home $data(SyncItems) }
-      share { create_sync_dir $data(SyncDirectory) $data(SyncItems) }
+      copy  { file_transfer $dirname $::tke_home $data(ShareItems) }
+      share { create_share_dir $data(ShareDirectory) $data(ShareItems) }
     }
 
-    # Create the sync.tkedat file
+    # Create the share.tkedat file
     write_file
 
   }
 
   ######################################################################
-  # Load the synchronization file.
+  # Load the sharing file.
   proc load_file {} {
 
     variable data
 
-    # Initialize the sync information
-    set data(SyncDirectory) ""
-    set data(SyncItems)     [list emmet launcher plugins prefs sessions snippets templates themes]
+    # Initialize the share information
+    set data(ShareDirectory) ""
+    set data(ShareItems)     [list emmet launcher plugins prefs sessions snippets templates themes]
 
-    # Read in the sync data from the file
-    if {![catch { [ns tkedat]::read [file join $::tke_home sync.tkedat] } rc]} {
+    # Read in the share data from the file
+    if {![catch { [ns tkedat]::read [file join $::tke_home share.tkedat] } rc]} {
       array set data $rc
     }
 
     # Update all affected namespaces
-    sync_changed
+    share_changed
 
   }
 
   ######################################################################
-  # Writes the synchronization file.
+  # Writes the sharing file.
   proc write_file {} {
 
     variable data
 
-    [ns tkedat]::write [file join $::tke_home sync.tkedat] [array get data] 0
+    [ns tkedat]::write [file join $::tke_home share.tkedat] [array get data] 0
 
   }
 
   ######################################################################
-  # Sets up the sync settings for editing in a buffer.
+  # Sets up the share settings for editing in a buffer.
   proc edit_setup {} {
 
     variable data
 
     # Create the buffer
-    [ns gui]::add_buffer end [msgcat::mc "Sync Setup"] [list [ns sync]::save_buffer_contents] -lang tkeData
+    [ns gui]::add_buffer end [msgcat::mc "Sharing Setup"] [list [ns share]::save_buffer_contents] -lang tkeData
 
     # Get the newly added buffer
     set txt [[ns gui]::current_txt {}]
 
-    # Get the contents from the base sync file
-    if {![catch { [ns tkedat]::read [file join $::tke_dir data sync.tkedat] } rc]} {
+    # Get the contents from the base sharing file
+    if {![catch { [ns tkedat]::read [file join $::tke_dir data share.tkedat] } rc]} {
       array set contents $rc
     }
 
-    foreach opt [list SyncDirectory SyncItems] {
+    foreach opt [list ShareDirectory ShareItems] {
       set contents($opt) $data($opt)
     }
 
@@ -465,7 +464,7 @@ namespace eval sync {
   }
 
   ######################################################################
-  # Called when the synchronization text file is saved.
+  # Called when the sharing text file is saved.
   proc save_buffer_contents {file_index} {
 
     # Get the current buffer
@@ -475,7 +474,7 @@ namespace eval sync {
     array set data [[ns tkedat]::parse [[ns gui]::scrub_text $txt] 0]
 
     # Indicate that values may have changed
-    save_changes $data(SyncDirectory) $data(SyncItems)
+    save_changes $data(ShareDirectory) $data(ShareItems)
 
     return 0
 
