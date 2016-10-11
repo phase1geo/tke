0b9c62184aa43ad7ef31bd45a3550ba1009fc446
Trevor Williams <phase1geo@users.sourceforge.net>
2016-10-10 22:51:52 -0500

    * Checkpointing work on ftper namespace (opening, getting and saving are working).

--- lib/ftptest.tcl
+++ lib/ftptest.tcl
@@ -5,14 +5,26 @@ lappend auto_path [pwd]
 
 package require ftp
 package require tablelist
-package require Expect
+# package require Expect
 
 source ftper.tcl
 source utils.tcl
 
 ttk::style theme use clam
 
-puts "File to open: [ftper::create_open]"
+wm attributes . -alpha 0.0
+
+lassign [ftper::create_open] name fname
+
+puts "name: $name, fname: $fname"
+
+if {$name ne ""} {
+  if {[ftper::get_file $name $fname ::contents]} {
+    puts $contents
+  } else {
+    puts "No file downloaded"
+  }
+}
 
 exit
 
