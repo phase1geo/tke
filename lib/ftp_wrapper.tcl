1687b757a5f24bce8a600a439a23bd49da9795c3
Trevor Williams <phase1geo@gmail.com>
2016-11-15 12:32:58 -0600

    * Removing obsolete sftp directory. * Fixing ftp_wrapper to still allow FTP support if Expect is not found.

--- lib/ftp_wrapper.tcl
+++ lib/ftp_wrapper.tcl
@@ -23,99 +23,96 @@
 #          work without modification.
 ######################################################################
 
-# Only provide SFTP support if Expect is installed/available
-if {![catch { package require Expect }]} {
-
-  ######################################################################
-  # This procedure is called by the sftp.tcl procedures.  We will ignore
-  # their parameters.
-  proc ::frputs {args} {
-    return
-    set m {}
-    foreach ar  $args {
-      if {[string index $ar end] == " " } {
-        set m [set m][string range $ar 0 end-1]
-      } elseif { ! [catch "uplevel \"info exists $ar\" " ro] &&  $ro } {
-        set m "[set m]$ar=[uplevel "set $ar"]< "
-      } else {
-        set m "[set m]$ar=<unset> "
-      }
-    }
-    regsub -all {\n} $m {\\n} m
-    regsub -all {\r} $m {\\r} m
-    regsub -all {\t} $m {\\t} m
-    puts "frputs: [set m]"
-    flush stdout
-  }
-
-  ######################################################################
-  # This procedure is used by the sftp code.
-  proc ::Log {str} {
-    # puts "Log: $str"
-  }
-
-  ######################################################################
-  # This procedure is used by the sftp code.
-  proc ::LogStatusOnly {str} {
-    if {[::tke_development]} {
-      puts "LogStatusOnly: $str"
+######################################################################
+# This procedure is called by the sftp.tcl procedures.  We will ignore
+# their parameters.
+proc ::frputs {args} {
+  return
+  set m {}
+  foreach ar  $args {
+    if {[string index $ar end] == " " } {
+      set m [set m][string range $ar 0 end-1]
+    } elseif { ! [catch "uplevel \"info exists $ar\" " ro] &&  $ro } {
+      set m "[set m]$ar=[uplevel "set $ar"]< "
+    } else {
+      set m "[set m]$ar=<unset> "
     }
   }
+  regsub -all {\n} $m {\\n} m
+  regsub -all {\r} $m {\\r} m
+  regsub -all {\t} $m {\\t} m
+  puts "frputs: [set m]"
+  flush stdout
+}
 
-  ######################################################################
-  # This procedure is used by the ftp_control code.
-  proc ::LogSilent {str} {
-    # puts "LogSilent: $str"
+######################################################################
+# This procedure is used by the sftp code.
+proc ::Log {str} {
+  # puts "Log: $str"
+}
+
+######################################################################
+# This procedure is used by the sftp code.
+proc ::LogStatusOnly {str} {
+  if {[::tke_development]} {
+    puts "LogStatusOnly: $str"
   }
+}
 
-  ######################################################################
-  # Required by ftp_control.
-  proc PopWarn { warn } {
-    logger::log $warn
-    # puts "warn: $warn"
-    # smart_dialog .apop . [_ "Warning"] [list $warn] 0 1 [_ "OK"]
-    # LogStatusOnly "[lindex [split $warn \n] 0]"
-    # LogSilent [_ "**Warning**\n%s" $warn]
-  }
+######################################################################
+# This procedure is used by the ftp_control code.
+proc ::LogSilent {str} {
+  # puts "LogSilent: $str"
+}
 
-  ######################################################################
-  # Required by sftp
-  proc ::_ {s {p1 ""} {p2 ""} {p3 ""} {p4 ""}} {
+######################################################################
+# Required by ftp_control.
+proc PopWarn { warn } {
 
-    return [::msgcat::mc $s $p1 $p2 $p3 $p4]
-
-  }
-
-  ######################################################################
-  # Required by sftp.
-  proc ::smart_dialog {win parent title lines args} {
-
-    set ans [tk_messageBox -parent .ftp -title $title -message [lindex $lines 2] -detail [lindex $lines 1] -default yes -type yesno]
-
-    return [expr {($ans eq "yes") ? 1 : 2}]
-
-  }
-
-  ######################################################################
-  # Returns the current time in milliseconds.  Required by ftp_control.tcl
-  proc ClockMilliSeconds {} {
-
-    return [clock milliseconds]
-
-  }
-
-  set ::glob(debug)                  0
-  set ::glob(ftp,debug)              0
-  set ::glob(os)                     "Unix"  ;# TBD
-  set ::glob(abortcmd)               0
-  set ::config(ftp,timeout)          60
-  set ::config(ftp,cache,maxentries) 0  ;# Effectively turn off caching
-  set ::ftp(cache)                   [list]
-
-  # Load the sftp code base
-  source [file join $tke_dir lib remote ftp.tcl]
-  source [file join $tke_dir lib remote sftp.tcl]
-  source [file join $tke_dir lib remote ftp_control.tcl]
+  logger::log $warn
 
 }
+
+######################################################################
+# Required by sftp
+proc ::_ {s {p1 ""} {p2 ""} {p3 ""} {p4 ""}} {
+
+  return [::msgcat::mc $s $p1 $p2 $p3 $p4]
+
+}
+
+######################################################################
+# Required by sftp.
+proc ::smart_dialog {win parent title lines args} {
+
+  set ans [tk_messageBox -parent .ftp -title $title -message [lindex $lines 2] -detail [lindex $lines 1] -default yes -type yesno]
+
+  return [expr {($ans eq "yes") ? 1 : 2}]
+
+}
+
+######################################################################
+# Returns the current time in milliseconds.  Required by ftp_control.tcl
+proc ClockMilliSeconds {} {
+
+  return [clock milliseconds]
+
+}
+
+set ::glob(debug)                  0
+set ::glob(ftp,debug)              0
+set ::glob(os)                     "Unix"  ;# TBD
+set ::glob(abortcmd)               0
+set ::config(ftp,timeout)          60
+set ::config(ftp,cache,maxentries) 0  ;# Effectively turn off caching
+set ::ftp(cache)                   [list]
+
+# Load the sftp code base only if we have Expect
+if {![catch { package require Expect }]} {
+  source [file join $tke_dir lib remote sftp.tcl]
+}
+
+# Load the FTP and FTP control files
+source [file join $tke_dir lib remote ftp.tcl]
+source [file join $tke_dir lib remote ftp_control.tcl]
 
