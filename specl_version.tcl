cc6c28f963a016089ee1b75f4abea3e0c4a999e9
Trevor Williams <phase1geo@users.sourceforge.net>
2017-01-13 00:15:14 -0600

    * Updating specl_version.tcl index value.

--- specl_version.tcl
+++ specl_version.tcl
@@ -1,5 +1,5 @@
 set specl::appname      "tke"
 set specl::version      "2.8.21"
-set specl::release      "305"
+set specl::release      "318"
 set specl::rss_url      "http://tke.sourceforge.net/releases"
 set specl::download_url "http://tke.sourceforge.net/releases"
