d56d4b27f2195628b0d4d7da1f88112e568a5659
Trevor Williams <phase1geo@users.sourceforge.net>
2017-01-12 19:05:20 -0600

    * Second attempt to fix specl package.

--- lib/specl/pkgIndex.tcl
+++ lib/specl/pkgIndex.tcl
@@ -1,8 +1,8 @@
 #
 # Tcl package index file
 #
-package ifneeded specl 2.0 {
+package ifneeded specl 2.0 [list apply { dir {
   namespace eval specl [list proc DIR {} [list return $dir]]
   source [file join $dir lib specl.tcl]
-}
+}} $dir]
 
