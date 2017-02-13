7834dfd484c2b81b48ce625603b458978636ff3a
Trevor Williams <phase1geo@users.sourceforge.net>
2017-02-13 08:22:26 -0600

    ChangeLog for devel-2.9.7 release

--- lib/version.tcl
+++ lib/version.tcl
@@ -1,4 +1,4 @@
 set version_major "2"
 set version_minor "9"
-set version_point "6"
-set version_hgid  "2825"
+set version_point "7"
+set version_hgid  "2826"
--- specl_version.tcl
+++ specl_version.tcl
@@ -1,5 +1,7 @@
 set specl::appname      "tke"
-set specl::version      "2.9.6"
-set specl::release      "327"
+set specl::version      "2.9.7"
+set specl::release      "Unable to insert into database: You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near 'else" keyword in Perl syntax.\</li\>
+\
+\<li\>Fixing issue with updating title ba' at line 1"
 set specl::rss_url      "http://tke.sourceforge.net/releases"
 set specl::download_url "http://tke.sourceforge.net/releases"
