0b69959558eaeb1c8eabc252e332914715b27e9e
Trevor Williams <phase1geo@users.sourceforge.net>
2017-03-01 00:01:22 -0600

    * Adding missing `const` keyword to JavaScript syntax. * Changing some syntax highlighting for TKE syntax files. * Adding new `HighlightEndProc` keyword in TKE syntax files to allow embedded Tcl syntax highlighting within `HighlightProc` procedures. * Enhancing key highlighting in YAML syntax. * Fixing previous character index calculations from previous checkin. * Adding ability to jump to a line at a certain percentage line within a file (i.e., `10%`) * Adding ability to jump to a specific line via the `G` vim command. * Adding more tests to Vim BIST suite.

--- tests/vim.tcl
+++ tests/vim.tcl
@@ -498,12 +498,12 @@ namespace eval vim {
 
   # Verify firstword motion
   proc run_test9 {} {
-    
+
     # Initialize
     set txtt [initialize].t
-    
+
     $txtt insert end "\n apples\n\ngrapes\n      bananas\n  milk\n\n    soup"
-    
+
     # Verify the current line firstword variety
     foreach index [list 1.0 2.1 3.0 4.0 5.6 6.2 7.0 8.4] {
       $txtt mark set insert "$index linestart"
@@ -514,11 +514,11 @@ namespace eval vim {
         cleanup "Current firstword is at incorrect position ([$txtt index insert])"
       }
     }
-    
+
     $txtt mark set insert 1.0
     vim::adjust_insert $txtt
     set vim::number($txtt) ""
-    
+
     # Test nextfirst (one line at a time)
     foreach index [list 2.1 3.0 4.0 5.6 6.2 7.0 8.4 8.4] {
       vim::handle_Return $txtt
@@ -526,7 +526,7 @@ namespace eval vim {
         cleanup "Next firstword is at incorrect position ([$txtt index insert])"
       }
     }
-    
+
     # Test prevfirst (one line at a time)
     foreach index [list 7.0 6.2 5.6 4.0 3.0 2.1 1.0 1.0] {
       vim::handle_minus $txtt
@@ -534,9 +534,9 @@ namespace eval vim {
         cleanup "Previous firstword is at incorrect position ([$txtt index insert])"
       }
     }
-    
+
     set vim::number($txtt) 2
-    
+
     # Test nextfirst (two lines at a time)
     foreach index [list 3.0 5.6 7.0 8.4] {
       vim::handle_Return $txtt
@@ -544,7 +544,7 @@ namespace eval vim {
         cleanup "Next firstword is at incorrect position ([$txtt index insert])"
       }
     }
-    
+
     # Test prevfirst (two lines at a time)
     foreach index [list 6.2 4.0 2.1 1.0] {
       vim::handle_minus $txtt
@@ -552,10 +552,282 @@ namespace eval vim {
         cleanup "Previous firstword is at incorrect position ([$txtt index insert])"
       }
     }
-    
+
     # Cleanup
     cleanup
-    
+
+  }
+
+  # Next/previous character motion
+  proc run_test10 {} {
+
+    # Initialize
+    set txtt [initialize].t
+
+    $txtt insert end "\ngood\n\ngreat"
+
+    $txtt mark set insert 1.0
+    vim::adjust_insert $txtt
+
+    # Move forward through the document, one character at a time
+    foreach index [list 2.0 2.1 2.2 2.3 3.0 4.0 4.1 4.2 4.3 4.4 4.4] {
+      vim::handle_space $txtt
+      if {[$txtt index insert] ne $index} {
+        cleanup "Moving one character forward did not work properly ([$txtt index insert])"
+      }
+    }
+
+    # Move backward through the document, one character at a time
+    foreach index [list 4.3 4.2 4.1 4.0 3.0 2.3 2.2 2.1 2.0 1.0 1.0] {
+      vim::handle_BackSpace $txtt
+      if {[$txtt index insert] ne $index} {
+        cleanup "Moving one character backward did not work properly ([$txtt index insert])"
+      }
+    }
+
+    # Move forward 2 characters at a time
+    set vim::number($txtt) 2
+    foreach index [list 2.1 2.3 4.0 4.2 4.4 4.4] {
+      vim::handle_space $txtt
+      if {[$txtt index insert] ne $index} {
+        cleanup "Moving two characters forward did not work properly ([$txtt index insert])"
+      }
+    }
+
+    # Move backward through the document, one character at a time
+    foreach index [list 4.2 4.0 2.3 2.1 1.0 1.0] {
+      vim::handle_BackSpace $txtt
+      if {[$txtt index insert] ne $index} {
+        cleanup "Moving two characters backward did not work properly ([$txtt index insert])"
+      }
+    }
+
+    # Clean things up
+    cleanup
+
+  }
+
+  # Verify top, middle, bottom of screen
+  proc run_test11 {} {
+
+    # Initialize
+    set txtt [initialize].t
+
+    $txtt insert end [string repeat "\n" 100]
+
+    $txtt mark set insert 10.0
+    vim::adjust_insert $txtt
+
+    if {[$txtt index insert] ne "10.0"} {
+      cleanup "Starting insertion point was incorrect ([$txtt index insert])"
+    }
+
+    # Jump the cursor to the top of the screen
+    vim::handle_H $txtt
+    if {[$txtt index insert] ne [$txtt index @0,0]} {
+      cleanup "Top index was incorrect ([$txtt index insert])"
+    }
+
+    # Jump to the middle
+    vim::handle_M $txtt
+    if {[$txtt index insert] ne [$txtt index @0,[expr [winfo reqheight $txtt] / 2]]} {
+      cleanup "Middle index was incorrect ([$txtt index insert])"
+    }
+
+    # Jump to the bottom
+    vim::handle_L $txtt
+    if {[$txtt index insert] ne [$txtt index @0,[winfo reqheight $txtt]]} {
+      cleanup "Bottom index was incorrect ([$txtt index insert])"
+    }
+
+    # Jump to the last line
+    vim::handle_G $txtt
+    if {[$txtt index insert] ne "101.0"} {
+      cleanup "Bottom of file was incorrect ([$txtt index insert])"
+    }
+
+    # Jump to a specific line
+    set vim::number($txtt) 12
+    vim::handle_G $txtt
+    if {[$txtt index insert] ne "12.0"} {
+      cleanup "Jumping to a specific line was incorrect ([$txtt index insert])"
+    }
+
+    # Jump to a line percentage
+    set vim::number($txtt) 20
+    vim::handle_percent $txtt
+    if {[$txtt index insert] ne "21.0"} {
+      cleanup "Jumping to a percentage of the file was incorrect ([$txtt index insert])"
+    }
+
+    # Clean things up
+    cleanup
+
+  }
+
+  # Character search motion
+  proc run_test12 {} {
+
+    # Initialize
+    set txtt [initialize].t
+
+    $txtt insert end "\nThis is really strong\n\nBut I am stronger"
+
+    # Attempt to find a character that does not exist
+    $txtt mark set insert 2.0
+    vim::adjust_insert $txtt
+    vim::handle_f $txtt
+    vim::handle_any $txtt x x
+    if {[$txtt index insert] ne "2.0"} {
+      cleanup "We should not have found anything with f ([$txtt index insert])"
+    }
+
+    # Search forward for the first l
+    $txtt mark set insert 2.0
+    vim::adjust_insert $txtt
+    vim::handle_f $txtt
+    vim::handle_any $txtt l l
+    if {[$txtt index insert] ne "2.11"} {
+      cleanup "Search for next l is incorrect f ([$txtt index insert])"
+    }
+
+    # Search forward for the second l
+    $txtt mark set insert 2.0
+    vim::adjust_insert $txtt
+    vim::handle_number $txtt 2
+    vim::handle_f $txtt
+    vim::handle_any $txtt l l
+    if {[$txtt index insert] ne "2.12"} {
+      cleanup "Search for second l is incorrect f ([$txtt index insert])"
+    }
+
+    # Search forward for the third l
+    $txtt mark set insert 2.0
+    vim::adjust_insert $txtt
+    vim::handle_number $txtt 3
+    vim::handle_f $txtt
+    vim::handle_any $txtt l l
+    if {[$txtt index insert] ne "2.0"} {
+      cleanup "Search for third l is incorrect f ([$txtt index insert])"
+    }
+
+    # Attempt to find a character that does not exist
+    $txtt mark set insert 2.0
+    vim::adjust_insert $txtt
+    vim::handle_t $txtt
+    vim::handle_any $txtt x x
+    if {[$txtt index insert] ne "2.0"} {
+      cleanup "We should not have found anything with t ([$txtt index insert])"
+    }
+
+    # Search forward for the first l
+    $txtt mark set insert 2.0
+    vim::adjust_insert $txtt
+    vim::handle_t $txtt
+    vim::handle_any $txtt l l
+    if {[$txtt index insert] ne "2.10"} {
+      cleanup "Search for next l is incorrect t ([$txtt index insert])"
+    }
+
+    # Search forward for the second l
+    $txtt mark set insert 2.0
+    vim::adjust_insert $txtt
+    vim::handle_number $txtt 2
+    vim::handle_t $txtt
+    vim::handle_any $txtt l l
+    if {[$txtt index insert] ne "2.11"} {
+      cleanup "Search for second l is incorrect t ([$txtt index insert])"
+    }
+
+    # Search forward for the third l
+    $txtt mark set insert 2.0
+    vim::adjust_insert $txtt
+    vim::handle_number $txtt 3
+    vim::handle_t $txtt
+    vim::handle_any $txtt l l
+    if {[$txtt index insert] ne "2.0"} {
+      cleanup "Search for third l is incorrect t ([$txtt index insert])"
+    }
+
+    # Attempt to find a character that does not exist
+    $txtt mark set insert 2.end
+    vim::adjust_insert $txtt
+    vim::handle_F $txtt
+    vim::handle_any $txtt x x
+    if {[$txtt index insert] ne "2.20"} {
+      cleanup "We should not have found anything with F ([$txtt index insert])"
+    }
+
+    # Search forward for the first l
+    $txtt mark set insert 2.end
+    vim::adjust_insert $txtt
+    vim::handle_F $txtt
+    vim::handle_any $txtt l l
+    if {[$txtt index insert] ne "2.12"} {
+      cleanup "Search for next l is incorrect F ([$txtt index insert])"
+    }
+
+    # Search forward for the second l
+    $txtt mark set insert 2.end
+    vim::adjust_insert $txtt
+    vim::handle_number $txtt 2
+    vim::handle_F $txtt
+    vim::handle_any $txtt l l
+    if {[$txtt index insert] ne "2.11"} {
+      cleanup "Search for second l is incorrect F ([$txtt index insert])"
+    }
+
+    # Search forward for the third l
+    $txtt mark set insert 2.end
+    vim::adjust_insert $txtt
+    vim::handle_number $txtt 3
+    vim::handle_F $txtt
+    vim::handle_any $txtt l l
+    if {[$txtt index insert] ne "2.20"} {
+      cleanup "Search for third l is incorrect F ([$txtt index insert])"
+    }
+
+    # Attempt to find a character that does not exist
+    $txtt mark set insert 2.end
+    vim::adjust_insert $txtt
+    vim::handle_T $txtt
+    vim::handle_any $txtt x x
+    if {[$txtt index insert] ne "2.20"} {
+      cleanup "We should not have found anything with T ([$txtt index insert])"
+    }
+
+    # Search forward for the first l
+    $txtt mark set insert 2.end
+    vim::adjust_insert $txtt
+    vim::handle_T $txtt
+    vim::handle_any $txtt l l
+    if {[$txtt index insert] ne "2.13"} {
+      cleanup "Search for next l is incorrect T ([$txtt index insert])"
+    }
+
+    # Search forward for the second l
+    $txtt mark set insert 2.end
+    vim::adjust_insert $txtt
+    vim::handle_number $txtt 2
+    vim::handle_T $txtt
+    vim::handle_any $txtt l l
+    if {[$txtt index insert] ne "2.12"} {
+      cleanup "Search for second l is incorrect T ([$txtt index insert])"
+    }
+
+    # Search forward for the third l
+    $txtt mark set insert 2.end
+    vim::adjust_insert $txtt
+    vim::handle_number $txtt 3
+    vim::handle_T $txtt
+    vim::handle_any $txtt l l
+    if {[$txtt index insert] ne "2.20"} {
+      cleanup "Search for third l is incorrect T ([$txtt index insert])"
+    }
+
+    # Clean things up
+    cleanup
+
   }
 
 }
