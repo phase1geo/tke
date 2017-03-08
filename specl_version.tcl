e1a896abbe010f545b329f810e0b943ccf62ebb9
Trevor Williams <phase1geo@users.sourceforge.net>
2017-03-07 22:01:31 -0600

    ChangeLog for devel-2.9.11 release

--- specl_version.tcl
+++ specl_version.tcl
@@ -1,5 +1,6 @@
 set specl::appname      "tke"
-set specl::version      "2.9.10"
-set specl::release      "329"
+set specl::version      "2.9.11"
+set specl::release      "Unable to insert into database: You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near 'Close Other Tabs in Pane" and "Close All Tabs in Pane" tab menu options.\</li\>
+' at line 1"
 set specl::rss_url      "http://tke.sourceforge.net/releases"
 set specl::download_url "http://tke.sourceforge.net/releases"
