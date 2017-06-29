a1ae2311c9bedae542fb43d75805cb82f0f1e4a7
Trevor Williams <phase1geo@users.sourceforge.net>
2017-06-28 21:50:15 -0500

    * Updating tablelist from version 5.17 to 5.18.

--- lib/tke.tcl
+++ lib/tke.tcl
@@ -55,7 +55,7 @@ proc tke_development {} {
 wm withdraw .
 
 set auto_path [list [file join $tke_dir lib ctext] \
-                    [file join $tke_dir lib tablelist5.17] \
+                    [file join $tke_dir lib tablelist5.18] \
                     [file join $tke_dir lib ptwidgets1.2] \
                     [file join $tke_dir lib specl] \
                     [file join $tke_dir lib webdav] \
@@ -68,7 +68,7 @@ if {$tcl_platform(platform) eq "windows"} {
   package require Tclx
 }
 package require -exact ctext 5.0
-package require -exact tablelist 5.17
+package require -exact tablelist 5.18
 package require tooltip
 package require msgcat
 package require tokenentry
