d088e761fbc5e827df1a0be8f1a6270b10e7b01d
Trevor Williams <phase1geo@users.sourceforge.net>
2017-08-11 02:12:30 -0500

    * Fixing specl_version.tcl

--- lib/version.tcl
+++ lib/version.tcl
@@ -1,4 +1,4 @@
 set version_major "3"
 set version_minor "2"
 set version_point "7"
-set version_hgid  "3414"
+set version_hgid  "3416"
--- specl_version.tcl
+++ specl_version.tcl
@@ -1,7 +1,5 @@
 set specl::appname      "tke"
 set specl::version      "3.2.7"
-set specl::release      "Unable to insert into database: You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near 'Get Info" menu option to sidebar contextual menus.\</li\>
-\
-\<li\>Adding ability' at line 1"
+set specl::release      "360"
 set specl::rss_url      "http://tke.sourceforge.net/releases"
 set specl::download_url "http://tke.sourceforge.net/releases"
