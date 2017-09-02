0d159b652f759f81a54120034e2f5d80e01ca6ba
Trevor Williams <phase1geo@users.sourceforge.net>
2017-09-02 00:29:36 -0500

    * Fixing.

--- specl_version.tcl
+++ specl_version.tcl
@@ -1,7 +1,5 @@
 set specl::appname      "tke"
-set specl::version      "3.3"
-set specl::release      "Unable to insert into database: You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near 'Credits" text font to match the rest of the window.\</li\>
-\
-\<li\>Cleaned up va' at line 1"
+set specl::version      "3.2.10"
+set specl::release      "363"
 set specl::rss_url      "http://tke.sourceforge.net/releases"
 set specl::download_url "http://tke.sourceforge.net/releases"
