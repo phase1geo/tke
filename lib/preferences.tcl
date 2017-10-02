c978889595f73f3c610b315dc14ffc7088c86705
Trevor Williams <phase1geo@gmail.com>
2017-10-02 14:00:37 -0500

    * Fixing issue with installing a plugin with preferences without restarting the application. * Increasing width of search field in the preferences window.

--- lib/preferences.tcl
+++ lib/preferences.tcl
@@ -396,12 +396,15 @@ namespace eval preferences {
         unset plugin_prefs($name)
       }
     }
-
+    
     # Add any new plugin preference values
     array set global_prefs [array get plugin_prefs]
-
+    
     # Replace the global user preferences
     set loaded_prefs(user,global) [array get global_prefs]
+    
+    # Update the global user preferences
+    update_prefs
 
   }
 
