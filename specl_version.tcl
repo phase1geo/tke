dc5cc3a8907c310f7555e59a87934727eb3ab345
Trevor Williams <phase1geo@users.sourceforge.net>
2017-10-06 01:46:34 -0500

    ChangeLog for devel-3.3.4 release

--- specl_version.tcl
+++ specl_version.tcl
@@ -1,5 +1,5 @@
 set specl::appname      "tke"
 set specl::version      "3.3.4"
-set specl::release      "367"
+set specl::release      "368"
 set specl::rss_url      "http://tke.sourceforge.net/releases"
 set specl::download_url "http://tke.sourceforge.net/releases"
