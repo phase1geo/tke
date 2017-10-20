6e7c039a5fd078c96000b77fbb2f1c2ca44e8615
Trevor Williams <phase1geo@gmail.com>
2017-10-20 17:18:20 -0500

    * Checkpointing.

--- lib/ctext/model_test.tcl
+++ lib/ctext/model_test.tcl
@@ -1,12 +1,40 @@
-source model.tcl
+package require Thread
 
-puts "HERE B"
+source model.tcl
+source parsers.tcl
+
+model::create [set txt "foo"]
+
+if {[catch { open parsers.tcl r } rc]} {
+  puts "ERROR:  Unable to read parsers.tcl"
+}
+
+set contents [read $rc]
+close $rc
+
+lappend bracketlist $txt,config,matchChar,,curly  1
+lappend bracketlist $txt,config,matchChar,,square 1
+
+puts [time { parsers::positionals [thread::id] $txt $contents 1 $bracketlist }]
+model::debug_show $txt
 
 # Create the tree
+if {0} {
 model::create foo
-puts "HERE A"
 model::debug_show foo
 
-model::insert foo {paren start 1.0 paren start 1.1 paren end 1.4 paren end 1.6}
-
+puts [time {model::insert foo {paren start 1.0 paren start 1.1 paren end 1.4 paren end 1.6 paren end 1.10} 0}]
 model::debug_show foo
+
+model::adjust_insert_indices foo 1.2 2.3
+model::debug_show foo
+
+model::insert foo {paren start 1.7 paren end 1.9} 0
+model::debug_show foo
+
+model::insert foo {paren start 1.8} 0
+model::debug_show foo
+
+model::insert foo {paren end 1.20 paren end 1.25} 0
+model::debug_show foo
+}
