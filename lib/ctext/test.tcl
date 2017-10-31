8ca6147564dbcfbc08963eef14953cbc8ed88ca9
Trevor Williams <phase1geo@users.sourceforge.net>
2017-10-31 01:00:26 -0500

    * Checkpointing work on integrating new parsers and model into ctext6.

---
+++ lib/ctext/test.tcl
@@ -0,0 +1,13 @@
+lappend auto_path [pwd]
+
+package require ctext 6.0
+
+pack [ctext .t] -fill both -expand yes
+
+# Initialize the ctext widget
+ctext::initialize .t
+
+ctext::addHighlightClass    .t keywords "red"
+ctext::addHighlightKeywords .t {hello trevor} class keywords
+ctext::setContextPatterns   .t bcomment comment "" {{/\*} {\*/}} "grey"
+ctext::setBrackets          .t "" {curly square paren double single} "green"
