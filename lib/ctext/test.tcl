e85c098f9420dd76d2be1d79226b96705839e13c
Trevor Williams <phase1geo@users.sourceforge.net>
2017-11-02 00:47:10 -0500

    * Checkpointing.

--- lib/ctext/test.tcl
+++ lib/ctext/test.tcl
@@ -11,3 +11,13 @@ ctext::addHighlightClass    .t keywords "red"
 ctext::addHighlightKeywords .t {hello trevor} class keywords
 ctext::setContextPatterns   .t bcomment comment "" {{{/\*} {\*/}}} "grey"
 ctext::setBrackets          .t "" {curly square paren double single} "green"
+
+source model.tcl
+
+proc show_tree {} {
+
+  model::load_all .t
+  model::debug_show_tree
+  model::destroy .t
+  
+}
