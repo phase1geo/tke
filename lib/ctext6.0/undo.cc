39349bd75b49e9de8aa60df7abe5672f34208749
Trevor Williams <phase1geo@gmail.com>
2017-11-18 23:42:14 -0600

    * Checkpointing undo buffer work.

--- lib/ctext6.0/undo.cc
+++ lib/ctext6.0/undo.cc
@@ -6,73 +6,145 @@
 
 #include "undo.h"
 
-void undo_manager::add_insert_change(
+void undo_change::render(
+  object result
+) const {
+
+  interpreter i( result.get_interp(), false );
+  object      item1, item2;
+
+  /* Render the insertion/deletion command */
+  if( _type == UNDO_TYPE_INSERT ) {
+    item1.append( i, (object)"insert" );
+    item1.append( i, (object)tindex_to_string( _startpos ) );
+    item1.append( i, (object)_str );
+  } else {
+    item1.append( i, (object)"delete" );
+    item1.append( i, (object)tindex_to_string( _startpos ) );
+    item1.append( i, (object)tindex_to_string( _endpos ) );
+  }
+  result.append( i, item1 );
+
+  /* Render the cursor positioning */
+  item2.append( i, (object)"mark" );
+  item2.append( i, (object)"set" );
+  item2.append( i, (object)"insert" );
+  item2.append( i, (object)tindex_to_string( _cursor ) );
+  result.append( i, item2 );
+
+}
+
+/* --------------------------------------------------- */
+
+void undo_group::render(
+  object & result
+) {
+
+  interpreter i( result.get_interp(), false );
+  undo_group  tmp = *this;
+
+  /* Clear ourselves */
+  clear();
+
+  /*
+   Move all of the items from the copied list back to ourselves, inverting
+   the data in the process.
+  */
+  while( tmp.top() ) {
+    tmp.top()->render( result );
+    tmp.top()->invert_type();
+    push( tmp.top() );
+    tmp.pop();
+  }
+
+}
+
+void undo_group::cursor_history(
+  object result
+) const {
+
+  interpreter i( result.get_interp(), false );
+
+  for( stack<undo_change*>::const_iterator it=begin(); it!=end(); it++ ) {
+    result.append( i, (object)tindex_to_string( (*it)->cursor() ) );
+  }
+
+}
+
+/* --------------------------------------------------- */
+
+object undo_buffer::cursor_history() const {
+
+  object result;
+
+  for( stack<undo_group*>::const_iterator it=begin(); it!=end(); it++ ) {
+    (*it)->cursor_history( result );
+  }
+
+  return( result );
+
+}
+
+/* --------------------------------------------------- */
+
+void undo_manager::add_change(
+  int    type,
   object startpos,
+  object endpos,
   object str,
-  object cursors
+  object cursor,
+  object mcursor
 ) {
   
-  interpreter    interp( startpos.get_interp(), false );
-  tindex         spos = object_to_tindex( startpos );
-  string         text = str.get<string>( interp );
-  vector<tindex> cursor_list;
-  
-  for( int i=0; i<cursors.length( interp ); i++ ) {
-    cursor_list.push_back( object_to_tindex( cursors.at( interp, i ) ) );
-  }
+  interpreter interp( startpos.get_interp(), false );
+  tindex      spos  = object_to_tindex( startpos );
+  tindex      epos  = object_to_tindex( endpos );
+  string      text  = str.get<string>( interp );
+  tindex      cur   = object_to_tindex( cursor );
+  bool        multi = mcursor.get<bool>( interp );
   
   /* If we don't have a current change group, create it now */
-  if( _group == 0 ) {
-    _group = new undo_group();
+  if( _uncommitted == 0 ) {
+    _uncommitted = new undo_group();
+    _redo.clear();
   }
   
   /* Add the change to the list */
-  _group->push( new insert_change( spos, text, cursor_list ) );
+  _uncommitted->push( new undo_change( type, spos, epos, text, cur, multi ) );
   
 }
 
-void undo_manager::add_delete_change(
-  object startpos,
-  object endpos,
-  object cursors
-) {
-  
-  interpreter    interp( startpos.get_interp(), false );
-  tindex         spos = object_to_tindex( startpos );
-  tindex         epos = object_to_tindex( endpos );
-  vector<tindex> cursor_list;
-  
-  for( int i=0; i<cursors.length( interp ); i++ ) {
-    cursor_list.push_back( object_to_tindex( cursors.at( interp, i ) ) );
+void undo_manager::add_separator() {
+
+  if( _uncommitted ) {
+    _undo_buffer.push( _uncommitted );
+    _uncommitted = 0;
   }
-  
-  /* If we don't have a current change group, create it now */
-  if( _group == 0 ) {
-    _group = new undo_group();
-  }
-  
-  /* Add the change to the list */
-  _group->push( new delete_change( spos, epos, cursor_list ) );
-  
+
 }
-    
-object undo() {
+
+object undo_manager::undo() {
   
   object result;
   
-  if( _group ) {
-    
-    /* Create the return result */
-    result = _group->render();
-    
-    /* Adding the current group to the redo buffer */
-    
-  } else if( _undo_buffer->top() ) {
-    
-    /* Create the return result */
-    result = _undo_buffer->top()->render();
-    
-    /* */
+  /* If we have uncommited changes, commit them now */
+  if( _uncommitted ) {
+    add_separator();
+  }
+ 
+  /* Only act on the undo buffer if there is something in it */
+  if( _undo_buffer.top() ) {
+
+    result = _undo_buffer.top()->render();
+
+    /* Invert all of the types */
+    _undo_buffer.front()->invert_types();
+
+    /* Push the undo buffer changes to the redo buffer */
+    _redo_buffer.push( _undo_buffer.top() );
+
+    /* Pop the undo buffer */
+    _undo_buffer.pop();
     
   }
   
@@ -80,19 +152,33 @@ object undo() {
   
 }
     
-object redo() {
+object undo_manager::redo() {
   
-  if( _group ) {
-    
-  } else {
-    
+  object result;
+
+  if( _redo_buffer.top() ) {
+
+    /* Generate the redo result */
+    result = _redo_buffer.front()->render();
+
+    /* Invert all of the types */
+    _redo_buffer.front()->invert_types();
+
+    /* Push the redo buffer changes to the undo buffer */
+    _undo_buffer.push( _redo_buffer.front() );
+
+    /* Remove the top-most entry from the redo buffer */
+    _redo_buffer.pop();
+
   }
-  
+
+  return( result );
+
 }
     
-object cursor_history() const {
+object undo_manager::cursor_history() const {
   
-  /* TBD */
+  return( _undo_buffer.cursor_history() );
   
 }
 
