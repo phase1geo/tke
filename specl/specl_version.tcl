e3cb26e5c685405d0f25d829ec69580663b49371
Trevor Williams <phase1geo@users.sourceforge.net>
2018-12-06 07:42:27 -0600

    ChangeLog for devel-3.5.3 release

--- specl/specl_version.tcl
+++ specl/specl_version.tcl
@@ -1,5 +1,7 @@
 set specl::appname      "tke"
-set specl::version      "3.5.2"
-set specl::release      "396"
+set specl::version      "3.5.3"
+set specl::release      "Unable to insert into database: You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'Sort By" sidebar submenu activation status in Linux environments.\</li\>
+\
+\</ul' at line 1"
 set specl::rss_url      "http://tke.sourceforge.net/releases"
 set specl::download_url "http://tke.sourceforge.net/releases"
