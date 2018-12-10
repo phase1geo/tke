750169d29453bfd364430218b7a60bd11cebc115
Trevor Williams <phase1geo@users.sourceforge.net>
2018-12-10 00:42:59 -0600

    ChangeLog for devel-3.5.4 release

--- specl/specl_version.tcl
+++ specl/specl_version.tcl
@@ -1,5 +1,5 @@
 set specl::appname      "tke"
-set specl::version      "3.5.3"
-set specl::release      "396"
+set specl::version      "3.5.4"
+set specl::release      "397"
 set specl::rss_url      "http://tke.sourceforge.net/releases"
 set specl::download_url "http://tke.sourceforge.net/releases"
