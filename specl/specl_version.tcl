e8d3a2ae1d663589b170d5d046d04da3f9676fd4
Trevor Williams <phase1geo@users.sourceforge.net>
2019-01-17 21:00:58 -0600

    ChangeLog for stable-3.6 release

--- specl/specl_version.tcl
+++ specl/specl_version.tcl
@@ -1,5 +1,7 @@
 set specl::appname      "tke"
-set specl::version      "3.5.9"
-set specl::release      "401"
+set specl::version      "3.6"
+set specl::release      "Unable to insert into database: You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'Show hidden files" preference option was set.\</li\>
+\
+\<li\>Fixed display of cu' at line 1"
 set specl::rss_url      "http://tke.sourceforge.net/releases"
 set specl::download_url "http://tke.sourceforge.net/releases"
