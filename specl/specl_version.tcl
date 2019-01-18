114863501795bb87db9f07e3ca14af642b8c8339
Trevor Williams <phase1geo@users.sourceforge.net>
2019-01-17 21:07:19 -0600

    * Fixing specl_version file.

--- specl/specl_version.tcl
+++ specl/specl_version.tcl
@@ -1,7 +1,5 @@
 set specl::appname      "tke"
 set specl::version      "3.6"
-set specl::release      "Unable to insert into database: You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'Show hidden files" preference option was set.\</li\>
-\
-\<li\>Fixed display of cu' at line 1"
+set specl::release      "401"
 set specl::rss_url      "http://tke.sourceforge.net/releases"
 set specl::download_url "http://tke.sourceforge.net/releases"
